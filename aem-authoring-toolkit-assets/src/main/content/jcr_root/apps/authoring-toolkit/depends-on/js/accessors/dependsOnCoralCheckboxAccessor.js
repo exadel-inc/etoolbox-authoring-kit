fb3c7e88e7c283aa97d2bb0938f6313f5f7e407f
ybernatskaya <ybernatskaya@exadel.com>
2020-02-03 15:01:35 +0300

    [DependsOn] Added '*' before a label in the required checkbox

---
+++ aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/depends-on/js/accessors/dependsOnCoralCheckboxAccessor.js
@@ -0,0 +1,20 @@
+/**
+ * @author Yana Bernatskaya (YanaBr)
+ * @version 2.2.3
+ *
+ * Coral 3 Checkbox accessor
+ * */
+(function ($, ns) {
+    const CHECKBOX_SELECTOR = '.coral3-Checkbox';
+    const CHECKBOX_LABEL_SELECTOR = '.coral3-Checkbox-description coral-checkbox-label';
+    const requiredMark = '* ';
+
+    ns.ElementAccessors.registerAccessor({
+        selector: `${CHECKBOX_SELECTOR}`,
+        required: function ($el, val) {
+            const $checkboxLabel = $el.find(CHECKBOX_LABEL_SELECTOR);
+            $checkboxLabel.text(requiredMark + $checkboxLabel.text());
+            ns.ElementAccessors.DEFAULT_ACCESSOR.required($el, val);
+        }
+    });
+})(Granite.$, Granite.DependsOnPlugin = (Granite.DependsOnPlugin || {}));
