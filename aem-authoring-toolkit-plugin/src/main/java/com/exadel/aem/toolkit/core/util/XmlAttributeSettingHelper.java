4e3ff216db32fb54b541b7b4a4e5ed2ea3244151
yminkevich <yminkevich@exadel.com>
2020-02-12 19:05:25 +0300

    Fixed Extends behaviour for a field with IgnoreValue annotation

--- aem-authoring-toolkit-plugin/src/main/java/com/exadel/aem/toolkit/core/util/XmlAttributeSettingHelper.java
+++ aem-authoring-toolkit-plugin/src/main/java/com/exadel/aem/toolkit/core/util/XmlAttributeSettingHelper.java
@@ -172,6 +172,9 @@ class XmlAttributeSettingHelper<T> {
         }
         String stringifiedValue = value != null ? value.toString() : null;
         if (StringUtils.isBlank(stringifiedValue) || stringifiedValue.equals(ignoredValue)) {
+            if (element.hasAttribute(name)){
+                element.removeAttribute(name);
+            }
             return;
         }
         setAttribute(element, valueType.equals(String.class)
