fd225cb70b2a839e0b529461c7cd6db766f8db2f
ybernatskaya <ybernatskaya@exadel.com>
2020-02-12 18:54:54 +0300

    [DependsOn] Repaired invalid ui for disabled fields

---
+++ aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/fixes/js/validation.selectors.js
@@ -0,0 +1,29 @@
+(function () {
+    const registry = $(window).adaptTo("foundation-registry");
+    const selectors = registry.get("foundation.validation.selector");
+
+    const OVERRIDES = [
+        {
+            condition: (selector) => selector.submittable === '.coral-RadioGroup',
+            rewrite: (selector) => selector.candidate = '.coral-RadioGroup:not([disabled])'
+        },
+        {
+            condition: (selector) => selector.submittable === '.cq-RichText,.cq-RichText-editable',
+            rewrite: (selector) => selector.candidate = '.cq-RichText:not([disabled]),.cq-RichText-editable:not([disabled])'
+        },
+        {
+            condition: (selector) => selector.submittable === 'coral-multifield',
+            rewrite: (selector) => selector.candidate = 'coral-multifield:not([disabled])'
+        },
+        {
+            condition: (selector) => selector.submittable === '.coral-Autocomplete:not(coral-autocomplete)',
+            rewrite: (selector) => selector.candidate = '.coral-Autocomplete:not(coral-autocomplete):not([disabled])'
+        }
+    ];
+
+    selectors.forEach((selector) => {
+        OVERRIDES
+            .filter((rule) => rule.condition(selector))
+            .forEach((rule) => rule.rewrite(selector));
+    });
+})();
