7c5ea51c4de17bad82956308f6867ec65d697565
acharniakou <alexey.chernyakov00@gmail.com>
2020-04-01 16:38:31 +0300

    [EDMTP-230] Added test case for heading widget

--- aem-authoring-toolkit-plugin/src/test/com/exadel/aem/toolkit/core/maven/WidgetsTest.java
+++ aem-authoring-toolkit-plugin/src/test/com/exadel/aem/toolkit/core/maven/WidgetsTest.java
@@ -36,6 +36,7 @@ import com.exadel.aem.toolkit.test.widget.ColorFieldWidget;
 import com.exadel.aem.toolkit.test.widget.HiddenWidget;
 import com.exadel.aem.toolkit.test.widget.PasswordWidget;
 import com.exadel.aem.toolkit.test.widget.TextAreaWidget;
+import com.exadel.aem.toolkit.test.widget.HeadingWidget;
 
 public class WidgetsTest extends DefaultTestBase {
 
@@ -137,5 +138,10 @@ public class WidgetsTest extends DefaultTestBase {
     @Test
     public void testPassword() {
         testComponent(PasswordWidget.class);
+    }
+
+    @Test
+    public void testHeading() {
+        testComponent(HeadingWidget.class);
     }
 }
