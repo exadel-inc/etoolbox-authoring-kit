e560a747fa7be2d2a045a1fc3304e4211ba327c3
Liubou Masiuk <lmasiuk@exadel.com>
2020-06-15 12:18:08 +0300

    [EDMTP-258] Add more verbose documentation for DependsOnActions and DependsOnRefTypes

--- aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnRefTypes.java
+++ aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnRefTypes.java
@@ -4,15 +4,33 @@ package com.exadel.aem.toolkit.api.annotations.assets.dependson;
  * Represents acceptable {@code dependsOnRef} values for use with {@link DependsOnRef} annotation
  */
 public enum DependsOnRefTypes {
+    /**
+     * Type will be chosen automatically based on element widget type
+     */
     AUTO {
         @Override
         public String toString() {
             return "";
         }
     },
+    /**
+     * No type cast is performed
+     */
     ANY,
+    /**
+     * Cast to boolean (according to JS cast rules)
+     */
     BOOLEAN,
+    /**
+     * Cast as a string value to boolean (true if string cast equals "true")
+     */
     BOOLSTRING,
+    /**
+     * Cast to number value
+     */
     NUMBER,
+    /**
+     * Cast to string
+     */
     STRING
 }
