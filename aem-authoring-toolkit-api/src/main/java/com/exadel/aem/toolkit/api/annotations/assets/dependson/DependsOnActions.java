3d8d0f27e968e83add7362bd9bd4b2e619a5b899
Liubou Masiuk <lmasiuk@exadel.com>
2020-06-15 12:18:08 +0300

    [EDMTP-258] Add more verbose documentation for DependsOnActions and DependsOnRefTypes

--- aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnActions.java
+++ aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnActions.java
@@ -6,14 +6,37 @@ package com.exadel.aem.toolkit.api.annotations.assets.dependson;
  */
 @SuppressWarnings("unused")
 public class DependsOnActions {
-    private DependsOnActions() {
-    }
+    /**
+     * Hides the element if the query result is 'falsy'
+     */
     public static final String VISIBILITY = "visibility";
+    /**
+     * Hides the tab or element's parent tab if the query result is 'falsy'
+     */
     public static final String TAB_VISIBILITY = "tab-visibility";
+    /**
+     * Sets the validation state of the field based on the query result
+     */
     public static final String VALIDATE = "validate";
+    /**
+     * Sets the 'required' marker of the field based on the query result
+     */
     public static final String REQUIRED = "required";
+    /**
+     * Sets the 'readonly' marker of the field based on the query result
+     */
     public static final String READONLY = "readonly";
+    /**
+     * Sets the field's disabled state based on the query result
+     */
     public static final String DISABLED = "disabled";
+    /**
+     * Sets the query result as field's value (undefined query result is skipped)
+     */
     public static final String SET = "set";
+    /**
+     * Sets the query result as field's value only if the current value is blank (undefined query result is skipped)
+     */
     public static final String SET_IF_BLANK = "set-if-blank";
+    private DependsOnActions() {}
 }
--- aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnRefTypes.java
+++ aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnRefTypes.java
@@ -5,15 +5,33 @@ package com.exadel.aem.toolkit.api.annotations.assets.dependson;
  */
 @SuppressWarnings("unused")
 public enum DependsOnRefTypes {
+    /**
+     * Type will be chosen automatically based on element widget type
+     */
     AUTO {
         @Override
         public String toString() {
             return "";
         }
     },
+    /**
+     * No type cast is performed
+     */
     ANY,
+    /**
+     * Cast to boolean (according to JS cast rules)
+     */
     BOOLEAN,
+    /**
+     * Cast as a string value to boolean (true if string cast equals "true")
+     */
     BOOLSTRING,
+    /**
+     * Cast to number value
+     */
     NUMBER,
+    /**
+     * Cast to string
+     */
     STRING
 }
