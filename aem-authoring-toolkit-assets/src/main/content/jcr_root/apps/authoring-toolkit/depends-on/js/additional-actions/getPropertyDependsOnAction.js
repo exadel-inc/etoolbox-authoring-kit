5cf09fd0ff5f2b3a43f8c344ca53dbe53bdbde59
YanaBr <ybernatskaya@exadel.com>
2020-07-01 20:39:12 +0300

    [EDMTP-246] PR fixes, cleanup

--- aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/depends-on/js/additional-actions/getPropertyDependsOnAction.js
+++ aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/depends-on/js/additional-actions/getPropertyDependsOnAction.js
@@ -17,42 +17,37 @@
         return dirMatches && dirMatches.length || 0;
     }
 
-    const RequestCache = (function (timeout) {
-        let clearTm = null;
-        let map = new Map();
+    class RequestCache {
+        static timeout = 2000;
+        static _clearTm = null;
+        static _map = new Map();
 
-        function clearCache() {
-            map.clear();
+        static clear() {
+            this._map.clear();
             console.debug('[DependsOn] Custom action "get-property" cache cleared.');
         }
 
-        function get(url) {
+        static get(url) {
             url = Granite.HTTP.externalize(url);
-            if (!map.has(url)) map.set(url, $.get(url));
-            if (clearTm) clearTimeout(clearTm);
-            clearTm = setTimeout(clearCache, timeout);
-            return map.get(url);
+            if (!this._map.has(url)) this._map.set(url, $.get(url));
+            if (this._clearTm) clearTimeout(this._clearTm);
+            this._clearTm = setTimeout(this.clear, this.timeout);
+            return this._map.get(url);
         }
-
-        return {get: get, clear: clearCache};
-    })(2000);
+    }
 
     function getParentProperty(path) {
         const $el = this.$el;
         const resourcePath = DependsOn.getDialogPath($el);
         const level = getLevel(path);
         const name = path.replace(PARENT_DIR_REGEX, '');
-        RequestCache.get(DependsOn.getNthParent(resourcePath, level) + '.infinity.json').then(
-            function (data) {
-                return DependsOn.get(data, name, '/');
-            },
-            function (e) {
+        RequestCache
+            .get(DependsOn.getNthParent(resourcePath, level) + '.infinity.json')
+            .then((data) => DependsOn.get(data, name, '/'), (e) => {
                 console.warn('Can not get data from node ' + resourcePath, e);
                 return '';
-            }
-        ).then(function (res) {
-            DependsOn.ElementAccessors.setValue($el, res);
-        });
+            })
+            .then((res) => DependsOn.ElementAccessors.setValue($el, res));
     }
     getParentProperty.cache = RequestCache;
     DependsOn.ActionRegistry.register('get-property', getParentProperty);
