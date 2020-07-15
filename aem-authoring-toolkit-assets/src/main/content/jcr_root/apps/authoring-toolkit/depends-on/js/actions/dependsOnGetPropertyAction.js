12a6adfa33e2196342b7c3009f6d79b8b7d7b259
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-07-16 01:46:25 +0300

    [DependsOn]: cleanup get-property action

--- aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/depends-on/js/actions/dependsOnGetPropertyAction.js
+++ aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/depends-on/js/actions/dependsOnGetPropertyAction.js
@@ -1,10 +1,11 @@
 /**
  * @author Alexey Stsefanovich (ala'n), Liubou Masiuk (liubou-masiuk), Yana Bernatskaya (YanaBr)
  * @version 2.2.4
+ *
  * Custom action to get component property
  * property path can be relative (e.g. 'node/nestedProperty' or '../../parentCompProperty')
  *
- * query - property path
+ * {string} query - property path
  * */
 
 (function (Granite, $, DependsOn) {
@@ -12,43 +13,43 @@
 
     const PARENT_DIR_REGEX = /\.\.\//g;
 
-    function getLevel(path) {
+    function getParentLevel(path) {
         const dirMatches = path.match(PARENT_DIR_REGEX);
         return dirMatches && dirMatches.length || 0;
     }
 
-    class RequestCache {
-        static timeout = 2000;
-        static _clearTm = null;
-        static _map = new Map();
-
-        static clear() {
-            RequestCache._map.clear();
-            console.debug('[DependsOn] Custom action "get-property" cache cleared.');
-        }
-
-        static get(url) {
-            url = Granite.HTTP.externalize(url);
-            if (!RequestCache._map.has(url)) RequestCache._map.set(url, $.get(url));
-            if (RequestCache._clearTm) clearTimeout(RequestCache._clearTm);
-            RequestCache._clearTm = setTimeout(RequestCache.clear, RequestCache.timeout);
-            return RequestCache._map.get(url);
-        }
+    function buildResourcePath($el, path) {
+        const parentLevel = getParentLevel(path);
+        const resourcePath = DependsOn.getDialogPath($el);
+        const targetPath = DependsOn.getNthParent(resourcePath, parentLevel);
+        return targetPath + '.infinity.json';
     }
 
+    /**
+     * Action definition
+     * @param {string} path - query
+     * */
     function getParentProperty(path) {
+        if (typeof path !== 'string') {
+            console.warn('[DependsOn]: can not execute \'get-property\', query should be a string');
+            return;
+        }
+
         const $el = this.$el;
-        const resourcePath = DependsOn.getDialogPath($el);
-        const level = getLevel(path);
         const name = path.replace(PARENT_DIR_REGEX, '');
-        RequestCache
-            .get(DependsOn.getNthParent(resourcePath, level) + '.infinity.json')
-            .then((data) => DependsOn.get(data, name, '/'), (e) => {
-                console.warn('Can not get data from node ' + resourcePath, e);
-                return '';
-            })
+        const requestPath = buildResourcePath($el, path);
+
+        DependsOn.RequestCache.instance
+            .get(requestPath)
+            .then(
+                (data) => DependsOn.get(data, name, '/'),
+                (e) => {
+                    console.warn('Can not get data from node ' + requestPath, e);
+                    return '';
+                }
+            )
             .then((res) => DependsOn.ElementAccessors.setValue($el, res));
     }
-    getParentProperty.cache = RequestCache;
+
     DependsOn.ActionRegistry.register('get-property', getParentProperty);
 })(Granite, Granite.$, Granite.DependsOnPlugin);
