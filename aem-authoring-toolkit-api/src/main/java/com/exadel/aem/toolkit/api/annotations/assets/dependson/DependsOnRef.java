9df520aa4ac776d332fdd852b3c3ffec4e9d51a4
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-07-16 15:57:18 +0300

    [DependsOn]: References now observing input event. "Lazy" references introduced to ignore rapid input event.

--- aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnRef.java
+++ aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/assets/dependson/DependsOnRef.java
@@ -26,4 +26,10 @@ public @interface DependsOnRef {
      * @return One of the predefined {@link DependsOnRefTypes} constants
      */
     DependsOnRefTypes type() default DependsOnRefTypes.AUTO;
+
+    /**
+     * Defines the 'dependsInRefLazy' attribute
+     * @return marker to make DependsOn reference lazy.
+     */
+    boolean lazy() default false;
 }
--- aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/depends-on/README.md
+++ aem-authoring-toolkit-assets/src/main/content/jcr_root/apps/authoring-toolkit/depends-on/README.md
@@ -42,6 +42,7 @@ For referenced field:
 
 * `data-dependsonref` - to mark a field, that is referenced from the Query.
 * `data-dependsonreftype` - (optional) to define expected type of reference value. 
+* `data-dependsonreflazy` - (marker) attribute to mark reference as lazy. DependsOn doesn't observe rapid events like `input`.
 
 #### DependsOn Usage
 
