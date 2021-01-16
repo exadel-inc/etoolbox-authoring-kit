a5fc5373200c11fc5cbc014a0cab22b1db6966d3
Liubou Masiuk <lmasiuk@exadel.com>
2021-01-16 13:36:07 +0300

    [EDMTP-300] PR feedback

--- ui.apps/src/main/content/jcr_root/apps/authoring-toolkit/custom-lists/components/content/itemWrapper/clientlibs/item-dialog/dialogLauncher.js
+++ ui.apps/src/main/content/jcr_root/apps/authoring-toolkit/custom-lists/components/content/itemWrapper/clientlibs/item-dialog/dialogLauncher.js
@@ -12,7 +12,7 @@
  * limitations under the License.
  */
 
-(function (ns) {
+(function (author, ns) {
     'use strict';
 
     const APPS_REGEXP = /^\/(apps\/)?/;
@@ -22,37 +22,32 @@
     ns.ACL.WRAPPER_RES_TYPE = 'authoring-toolkit/custom-lists/components/content/itemWrapper';
 
     ns.ACL.launchReferenceDialog = function (editable, itemPath) {
-        const dlg = new ns.author.ui.Dialog({
-            getConfig: getDialogConfig(editable, itemPath),
-            getRequestData: function () {
-                return { resourceType: ns.ACL.WRAPPER_RES_TYPE };
-            },
-            onSuccess: reloadEditable(editable)
+        const dialogConfig = getDialogConfig(editable, itemPath);
+        const dlg = new author.ui.Dialog({
+            getConfig: () => dialogConfig,
+            getRequestData: () => ({ resourceType: ns.ACL.WRAPPER_RES_TYPE }),
+            onSuccess: () => reloadEditable(editable)
         });
-        ns.author.DialogFrame.openDialog(dlg);
+        author.DialogFrame.openDialog(dlg);
     };
 
     function reloadEditable(editable) {
-        return function () {
-            ns.author.edit.EditableActions.REFRESH.execute(editable).done(function () {
-                ns.author.selection.select(editable);
-                editable.afterEdit();
+        author.edit.EditableActions.REFRESH.execute(editable).done(function () {
+            author.selection.select(editable);
+            editable.afterEdit();
 
-                const editableParent = ns.author.editables.getParent(editable);
-                editableParent && editableParent.afterChildEdit(editable);
-            });
-        };
+            const editableParent = author.editables.getParent(editable);
+            editableParent && editableParent.afterChildEdit(editable);
+        });
     }
 
     function getDialogConfig(editable, itemResType) {
         const dialogPath = '/mnt/override/apps/' + itemResType.replace(APPS_REGEXP, '') + '/_cq_dialog.html' + editable.path;
         const dialogUrl = ns.HTTP.externalize(dialogPath);
-        return function () {
-            return {
-                src: dialogUrl,
-                loadingMode: 'auto',
-                layout: 'auto'
-            };
+        return {
+            src: dialogUrl,
+            loadingMode: 'auto',
+            layout: 'auto'
         };
     }
-}(Granite));
+}(Granite.author, Granite));
