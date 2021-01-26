7b5320caf09772c8832bb8077eaccccde7147e41
Liubou Masiuk <lmasiuk@exadel.com>
2021-01-26 12:26:06 +0300

    [EDMTP-300] Add licence info

--- aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/lists/ListItem.java
+++ aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/annotations/lists/ListItem.java
@@ -1,6 +1,18 @@
-package com.exadel.aem.toolkit.api.annotations.lists;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.exadel.aem.toolkit.api.annotations.meta.PropertyMapping;
+package com.exadel.aem.toolkit.api.annotations.lists;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,7 +24,5 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@PropertyMapping
-@SuppressWarnings("unused")
 public @interface ListItem {
 }
