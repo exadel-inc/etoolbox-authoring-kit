ea286e8a27a3d776ac440f4d2d2d55145bb4a8a5
Stepan Miakchilo <smiakchilo@exadel.com>
2021-03-01 11:29:24 +0300

    [tech] Converted API module into API bundle

--- aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/lists/services/ListHelperService.java
+++ aem-authoring-toolkit-api/src/main/java/com/exadel/aem/toolkit/api/lists/services/ListHelperService.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import com.day.cq.commons.jcr.JcrConstants;
 
 import com.exadel.aem.toolkit.api.lists.models.SimpleListItem;
 
@@ -26,52 +28,91 @@ import com.exadel.aem.toolkit.api.lists.models.SimpleListItem;
  */
 public interface ListHelperService {
 
-    default List<Resource> getResourceList(String path) {
-        return getList(path, Resource.class);
+    /**
+     * Retrieves collection of Sling {@link Resource}s representing list entries stored under given {@code path}
+     * @param resourceResolver Sling {@code ResourceResolver} instance used to access the list
+     * @param path             JCR path of the items list
+     * @return List of resources. If the path provided is invalid or cannot be resolved, an empty list
+     * is returned
+     */
+    default List<Resource> getResourceList(ResourceResolver resourceResolver, String path) {
+        return getList(resourceResolver, path, Resource.class);
     }
 
     /**
      * Retrieves collection of {@link SimpleListItem} values representing list entries stored under given {@code path}
-     * @param path JCR path of the items list
+     * @param resourceResolver Sling {@code ResourceResolver} instance used to access the list
+     * @param path             JCR path of the items list
      * @return List of {@link SimpleListItem}s. If the path provided is invalid or cannot be resolved, an empty list
      * is returned
      */
-    default List<SimpleListItem> getList(String path) {
-        return getList(path, SimpleListItem.class);
+    default List<SimpleListItem> getList(ResourceResolver resourceResolver, String path) {
+        return getList(resourceResolver, path, SimpleListItem.class);
     }
 
     /**
      * Retrieves collection of items representing list entries stored under given {@code path} adapted to the provided
      * {@code itemType}
-     * @param path     JCR path of the items list
-     * @param itemType {@code Class} reference representing type of entries required
-     * @param <T>      Type of list entries, must be one adaptable from a Sling {@code Resource}
+     * @param resourceResolver Sling {@code ResourceResolver} instance used to access the list
+     * @param path             JCR path of the items list
+     * @param itemType         {@code Class} reference representing type of entries required
+     * @param <T>              Type of list entries, must be one adaptable from a Sling {@code Resource}
      * @return List of {@code <T>}-typed instances. If the path provided is invalid or cannot be resolved, or else
-     * a non-Sling model {@code itemType} is given, an empty list is returned
+     * a non-adaptable {@code itemType} is given, an empty list is returned
      */
-    <T> List<T> getList(String path, Class<T> itemType);
+    <T> List<T> getList(ResourceResolver resourceResolver, String path, Class<T> itemType);
+
+    /**
+     * Retrieves collection of list entries stored under given {@code path} that is transformed into a key-value map.
+     * The keys represent {@code jcr:title} property of the underlying resource while the values are the underlying
+     * resources themselves. If several items have the same {@code jcr:title}, the last one is effective
+     * @param resourceResolver Sling {@code ResourceResolver} instance used to access the list
+     * @param path             JCR path of the items list
+     * @return Map representing title-to-value pairs. If the path provided is invalid or cannot be resolved, an empty
+     * map is returned
+     */
+    default Map<String, Resource> getResourceMap(ResourceResolver resourceResolver, String path) {
+        return getMap(resourceResolver, path, JcrConstants.JCR_TITLE, Resource.class);
+    }
+
+    /**
+     * Retrieves collection of list entries stored under given {@code path} that is transformed into a key-value map.
+     * The keys represent the attribute of the underlying resources specified by the given {@code keyName} while the
+     * values are the underlying resources themselves. If several items have the same {@code jcr:title}, the last one
+     * is effective
+     * @param resourceResolver Sling {@code ResourceResolver} instance used to access the list
+     * @param path             JCR path of the items list
+     * @param keyName          Item resource property that holds the key of the resulting map
+     * @return Map representing title-to-value pairs. If the path provided is invalid or cannot be resolved, an empty
+     * map is returned
+     */
+    default Map<String, Resource> getResourceMap(ResourceResolver resourceResolver, String path, String keyName) {
+        return getMap(resourceResolver, path, keyName, Resource.class);
+    }
 
     /**
      * Retrieves collection of list entries stored under given {@code path} that is transformed into a key-value map.
      * The keys represent {@code jcr:title} property of the underlying resource while the value represents {@code value}
      * property. If several items have the same {@code jcr:title}, the last one is effective
-     * @param path JCR path of the items list
+     * @param resourceResolver Sling {@code ResourceResolver} instance used to access the list
+     * @param path             JCR path of the items list
      * @return Map representing title-to-value pairs. If the path provided is invalid or cannot be resolved, an empty
      * map is returned
      */
-    Map<String, String> getMap(String path);
+    Map<String, String> getMap(ResourceResolver resourceResolver, String path);
 
     /**
      * Retrieves collection of list entries stored under given {@code path} that is transformed into a key-value map.
      * Keys represent the attribute of the underlying resources specified by the given {@code keyName}. Values are the
      * underlying resources themselves as adapted to the provided {@code itemType} model. If several items have the same
      * key, the last one is effective
-     * @param path     JCR path of the items list
-     * @param keyName  Item resource property that holds the key of the resulting map
-     * @param itemType {@code Class} reference representing type of map values required
-     * @param <T>      Type of map values, must be one adaptable from a Sling {@code Resource}
+     * @param resourceResolver Sling {@code ResourceResolver} instance used to access the list
+     * @param path             JCR path of the items list
+     * @param keyName          Item resource property that holds the key of the resulting map
+     * @param itemType         {@code Class} reference representing type of map values required
+     * @param <T>              Type of map values, must be one adaptable from a Sling {@code Resource}
      * @return Map containing {@code <T>}-typed instances. If the path provided is invalid or cannot be resolved,
-     * or else a non-Sling model {@code itemType} is given, an empty map is returned
+     * or else a non-adaptable model {@code itemType} is given, an empty map is returned
      */
-    <T> Map<String, T> getMap(String path, String keyName, Class<T> itemType);
+    <T> Map<String, T> getMap(ResourceResolver resourceResolver, String path, String keyName, Class<T> itemType);
 }
