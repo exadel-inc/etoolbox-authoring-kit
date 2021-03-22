8cd743dbc15742fec98e6ec9e11f1cfac5525d8b
Stepan Miakchilo <smiakchilo@exadel.com>
2021-03-22 13:39:06 +0300

    [EDMTP-382] Added Target#removeAttribute() method

--- aem-authoring-toolkit-plugin/src/main/java/com/exadel/aem/toolkit/plugin/target/TargetImpl.java
+++ aem-authoring-toolkit-plugin/src/main/java/com/exadel/aem/toolkit/plugin/target/TargetImpl.java
@@ -426,6 +426,15 @@ public class TargetImpl extends AdaptationBase<Target> implements Target {
         return this;
     }
 
+    @Override
+    public Target attributes(Annotation value, Predicate<Method> filter) {
+        if (value == null) {
+            return this;
+        }
+        populateAnnotationProperties(value, filter == null ? method -> true : filter);
+        return this;
+    }
+
     public Target attributes(Element value) {
         if (value != null) {
             populateElementProperties(value);
@@ -434,12 +443,8 @@ public class TargetImpl extends AdaptationBase<Target> implements Target {
     }
 
     @Override
-    public Target attributes(Annotation value, Predicate<Method> filter) {
-        if (value == null) {
-            return this;
-        }
-        populateAnnotationProperties(value, filter == null ? method -> true : filter);
-        return this;
+    public void removeAttribute(String name) {
+        attributes.remove(name);
     }
 
     private void populateElementProperties(Element value) {
