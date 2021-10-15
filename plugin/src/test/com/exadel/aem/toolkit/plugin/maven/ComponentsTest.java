4c941d92f298e3fbb72c98163ad4f12ea300fc5d
Konyaka1 <alexey.chernyakov00@gmail.com>
2021-10-15 10:32:57 +0300

    [EDMTP-428] Added test case

--- plugin/src/test/com/exadel/aem/toolkit/plugin/maven/ComponentsTest.java
+++ plugin/src/test/com/exadel/aem/toolkit/plugin/maven/ComponentsTest.java
@@ -36,6 +36,7 @@ import com.exadel.aem.toolkit.test.component.ComponentWithTabsAndInnerClass;
 import com.exadel.aem.toolkit.test.component.ComponentWithTabsAsNestedClasses;
 import com.exadel.aem.toolkit.test.component.ComponentWithoutDialog;
 import com.exadel.aem.toolkit.test.component.InheritanceTestCases;
+import com.exadel.aem.toolkit.test.component.ReplaceTestCase;
 import com.exadel.aem.toolkit.test.component.WriteModeTestCases;
 import com.exadel.aem.toolkit.test.component.viewPattern.component1.ComplexComponentHolder;
 
@@ -98,6 +99,11 @@ public class ComponentsTest extends DefaultTestBase {
     @Test
     public void testInheritanceOverride() {
         test(InheritanceTestCases.Child.class, "component/inheritanceOverride");
+    }
+
+    @Test
+    public void testReplaceCase() {
+        test(ReplaceTestCase.Child.class, "component/replaceTest");
     }
 
     @Test
