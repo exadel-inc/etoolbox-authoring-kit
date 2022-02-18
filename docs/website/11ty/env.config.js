8191573ddeab2a3cf23207af1a83304ed245e3fe
Anna Barmina <anutik-barmina@mail.ru>
2022-02-18 20:27:36 +0100

    [Website]: Fix environment config

--- docs/website/11ty/env.config.js
+++ docs/website/11ty/env.config.js
@@ -1,12 +1,11 @@
-const package = require('../package.json')
+const version = process.env.npm_package_version;
+const env = process.argv.find(arg => arg.startsWith('--env='))?.split('=')[1];
+const isDev = env === 'development';
+const date = new Date();
 
-const version = package.version
-const isDev = package.isDev
-const date = new Date()
 const context = {isDev, version, date};
 
-
 module.exports = (config) => {
-   config.addGlobalData('env', context);
+  config.addGlobalData('env', context);
 };
-
+Object.assign(module.exports, context);
