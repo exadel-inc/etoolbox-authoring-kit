2169643f1f38d4f2a3625bce0c9867a2e91d754e
Konyaka1 <alexey.chernyakov00@gmail.com>
2022-02-21 12:56:13 +0300

    [EAK-154] Fixed bug with wrong primary types, when only allowed children presents

--- plugin/src/main/java/com/exadel/aem/toolkit/plugin/handlers/common/AllowedChildrenHandler.java
+++ plugin/src/main/java/com/exadel/aem/toolkit/plugin/handlers/common/AllowedChildrenHandler.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.exadel.aem.toolkit.plugin.handlers.common;
 
 import com.exadel.aem.toolkit.api.annotations.main.ac.AllowedChildren;
@@ -32,7 +45,9 @@ public class AllowedChildrenHandler implements Handler {
         }
         String json = toJson(allowedChildrenList, isEditConfig(target));
         target
+                .attribute(DialogConstants.PN_PRIMARY_TYPE, DialogConstants.NT_EDIT_CONFIG)
                 .getOrCreateTarget(DialogConstants.NN_LISTENERS)
+                .attribute(DialogConstants.PN_PRIMARY_TYPE, DialogConstants.NT_LISTENERS)
                 .attribute(DialogConstants.PN_UPDATE_COMPONENT_LIST, String.format(DialogConstants.VALUE_POLICY_RESOLVER_FORMAT, json));
     }
 
