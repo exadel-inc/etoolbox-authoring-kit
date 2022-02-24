407685a357139beb3f1afd4e4b285d79f3bc8236
alesun <alesun@exadel.com>
2022-02-24 16:25:21 +0300

    [TECH]: add dynamic navigation render

--- docs/website/11ty/markdown.lib.js
+++ docs/website/11ty/markdown.lib.js
@@ -1,16 +1,16 @@
 const MarkdownIt = require('markdown-it');
 const {highlight} = require('./prismjs.lib');
-const {resolveLink} = require('../helpers/resolveLink');
-const {setCorrectImageSrc} = require('../helpers/setCorrectImageSrc')
+// const {resolveLink} = require('../helpers/resolveLink');
+// const {setCorrectImageSrc} = require('../helpers/setCorrectImageSrc')
 const replaceSymbolAt = (str) =>{
   return str.startsWith('@') ? str.replace('@','') : str;
 }
 const slugify = (s)=>encodeURIComponent(replaceSymbolAt(String(s).trim().toLowerCase().replace(/\s+/g, '-')));
 
 const markdown = MarkdownIt({html: true, highlight, linkify:true});
-markdown.use(md=>resolveLink(md))
-.use(require('markdown-it-anchor'), {level:2, slugify, tabIndex:false})
-.use((md) =>setCorrectImageSrc(md));
+// markdown.use(md=>resolveLink(md))
+// .use(require('markdown-it-anchor'), {level:2, slugify, tabIndex:false})
+// .use((md) =>setCorrectImageSrc(md));
 
 
 module.exports = (config) => {
