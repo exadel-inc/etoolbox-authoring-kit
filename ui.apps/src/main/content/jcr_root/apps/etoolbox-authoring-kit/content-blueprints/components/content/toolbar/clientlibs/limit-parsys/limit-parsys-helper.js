bd00adafce27ad035ce0576e13e455f2f8fc5d13
nharshunova <nharshunova@exadel.com>
2022-03-09 16:05:23 +0300

    [EAK-280] Content Blueprint Template: add ui activities to limit template insert action

--- ui.apps/src/main/content/jcr_root/apps/etoolbox-authoring-kit/content-blueprints/components/content/toolbar/clientlibs/limit-parsys/limit-parsys-helper.js
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-authoring-kit/content-blueprints/components/content/toolbar/clientlibs/limit-parsys/limit-parsys-helper.js
@@ -11,14 +11,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function (author, ns) {
+(function ($document, author) {
     'use strict';
+    const INSERT_ACTION = 'INSERT';
+    const COMPONENTS_LIMIT = 1;
 
-    ns.EToolboxContentBlueprintsParsys = ns.EToolboxContentBlueprintsParsys || {}
+    let rootEditable;
+    let rootPath;
 
-    /** Limit parsys */
-    ns.EToolboxContentBlueprintsParsys.limitParsys = function () {
+    function templateInsertCondition(editableBefore) {
+        let canInsert = true;
+        const parentPath = editableBefore.getParentPath();
+        // Check if we are trying to insert to the root component
+        if (parentPath === rootPath) {
+            canInsert = isRootChildrenInLimit();
+        }
+        return canInsert;
+    }
 
-    };
+    function isRootChildrenInLimit() {
+        const children = author.editables.getChildren(rootEditable, false);
+        return (children.length <= COMPONENTS_LIMIT);
+    }
 
-}(Granite.author, Granite));
+    function toggleInsertParsys() {
+        const insertEditable = author.editables.find(rootPath + '/*')[0];
+        insertEditable && insertEditable.overlay.setVisible(isRootChildrenInLimit());
+    }
+
+    $document.on('cq-layer-activated', function (ev) {
+        if (ev.layer === 'Edit') {
+            rootEditable = author.editables.getRoot();
+            rootPath = rootEditable.path;
+
+            const action = author.edit.EditableActions[INSERT_ACTION];
+            const defaultInsertActionCondition = action.condition;
+            action.condition = function (editableBefore, componentPath, componentGroup) {
+                return templateInsertCondition(editableBefore) && defaultInsertActionCondition(editableBefore, componentPath, componentGroup);
+            };
+
+            // set initial visibility state
+            toggleInsertParsys();
+
+            rootEditable.afterChildInsert = function () {
+                toggleInsertParsys();
+            };
+
+            rootEditable.afterChildDelete = function () {
+                toggleInsertParsys();
+            };
+
+            $document.on('cq-overlays-repositioned', function () {
+                toggleInsertParsys();
+            });
+        }
+    });
+}($(document), Granite.author));
