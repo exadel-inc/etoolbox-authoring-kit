d3a69ac822635220e7bb62eb63b6cf97693bcd68
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2022-03-10 16:47:56 +0300

    [EAK-299]: fix formatting and update whitesource config

--- docs/website/src/polyfill-medium.ts
+++ docs/website/src/polyfill-medium.ts
@@ -4,7 +4,7 @@
 // IE, Edge < 18
 import '@webcomponents/custom-elements/custom-elements.min';
 
-// IE, Safari <12, Edge < 14
+// IE, Safari < 12, Edge < 14
 import 'intersection-observer/intersection-observer';
 
 import './polyfill-light';
