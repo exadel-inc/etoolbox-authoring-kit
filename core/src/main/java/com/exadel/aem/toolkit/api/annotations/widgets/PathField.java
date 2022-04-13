5c5b12db922538a494bdc0e5316cb43c73402587
Stepan Miakchilo <smiakchilo@exadel.com>
2022-04-13 21:49:32 +0300

    [EAK-314] Added more @PathField properties according to the spec

--- core/src/main/java/com/exadel/aem/toolkit/api/annotations/widgets/PathField.java
+++ core/src/main/java/com/exadel/aem/toolkit/api/annotations/widgets/PathField.java
@@ -22,6 +22,8 @@ import com.exadel.aem.toolkit.api.annotations.meta.AnnotationRendering;
 import com.exadel.aem.toolkit.api.annotations.meta.PropertyRendering;
 import com.exadel.aem.toolkit.api.annotations.meta.ResourceType;
 import com.exadel.aem.toolkit.api.annotations.meta.ResourceTypes;
+import com.exadel.aem.toolkit.api.annotations.meta.StringTransformation;
+import com.exadel.aem.toolkit.api.annotations.widgets.common.NodeFilter;
 
 /**
  * Used to set up
@@ -35,6 +37,22 @@ import com.exadel.aem.toolkit.api.annotations.meta.ResourceTypes;
 public @interface PathField {
 
     /**
+     * Maps to the {@code multiple} attribute of this Granite UI component's node. Used to set whether the user is able
+     * to make multiple selections
+     * @return True or false
+     */
+    @PropertyRendering(ignoreValues = "false")
+    boolean multiple() default false;
+
+    /**
+     * Maps to the {@code forceSelection} attribute of this Granite UI component's node. If set to true, forces the user
+     * to select only from the list of given options. Otherwise the user can pass an arbitrary value
+     * @return True or false
+     */
+    @PropertyRendering(ignoreValues = "false")
+    boolean forceSelection() default false;
+
+    /**
      * Maps to the {@code droppable} attribute of this Granite UI component's node. Indicates if assets can be dropped
      * on the {@code PathField}
      * @return True or false
@@ -42,6 +60,14 @@ public @interface PathField {
     @PropertyRendering(ignoreValues = "false")
     boolean droppable() default false;
 
+    /**
+     * Maps to the {@code deleteHint} attribute of this Granite UI component's node. If set to true, generates the <a
+     * href="https://sling.apache.org/documentation/bundles/manipulating-content-the-slingpostservlet-servlets-post.html#delete">
+     * {@code @Delete}</a> attribute for the HTTP request
+     * @return True or false
+     */
+    @PropertyRendering(ignoreValues = "true")
+    boolean deleteHint() default true;
 
     /**
      * When set to a non-blank string, maps to the {@code emptyText} attribute of this Granite UI component's node. Used
@@ -51,9 +77,33 @@ public @interface PathField {
     String emptyText() default "";
 
     /**
+     * Defines the URI template that returns the suggestion markup. The template supports the following variables:<br>
+     * {@code query} - the query entered by the user;<br>
+     * {@code offset} - the offset of the pagination;<br>
+     * {@code limit} - the limit of the pagination
+     * @return String value
+     */
+    String suggestionSrc() default "";
+
+    /**
+     * Defines the URI template that returns the picker markup. The template supports the {@code value} variable which
+     * refers to the value of the first item
+     * @return String value
+     */
+    String pickerSrc() default "";
+
+    /**
      * Maps to the {@code root} attribute of this Granite UI component's node. Used to define the root node from which
-     * PathField navigation starts
+     * {@code PathField} navigation starts
      * @return String value representing valid JCR path
      */
     String rootPath() default "/";
+
+    /**
+     * Specifies the filter applied to the suggestion and picker
+     * @return One of {@code NodeFilter} values
+     * @see NodeFilter
+     */
+    @PropertyRendering(transform = StringTransformation.CAMELCASE, ignoreValues = "hierarchyNotFile")
+    NodeFilter filter() default NodeFilter.HIERARCHY_NOT_FILE;
 }
