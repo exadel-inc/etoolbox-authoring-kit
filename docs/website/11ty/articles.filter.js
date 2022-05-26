4c4c764ad35a2c9cfeeeb243997e3b50684e4133
bhlynsky <bhlynskyi@exadel.com>
2022-05-26 13:47:09 +0300

    [Tech] Minor code style update

--- docs/website/11ty/articles.filter.js
+++ docs/website/11ty/articles.filter.js
@@ -1,11 +1,11 @@
 module.exports = (config) => {
   config.addFilter('articleContent', (text) => {
     const contentRegex = /<p.*>.*?<\/p>/ig;
-    return text.match(contentRegex).join(' ')
+    return text.match(contentRegex).join(' ');
   });
   config.addFilter('articleHeader', (text) => {
     const headerRegex = /<h2.*>.*?<\/h2>/ig;
-    return text.match(headerRegex)[0]
+    return text.match(headerRegex)[0];
   });
   config.addFilter('filterArticles',  (arr, linkToArticle) => {
     return arr.filter((article) => article.data.page.filePathStem === linkToArticle);
