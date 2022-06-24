53cc22d2e1413c37410cb0efd637aadf4be587a5
Bohdan Taradada <btaradada@exadel.com>
2022-06-24 15:05:32 +0300

    Review fixes + watch on rmdir

--- docs/website/build/copy.js
+++ docs/website/build/copy.js
@@ -10,21 +10,31 @@ const OUTPUT_DIR = path.resolve(__dirname, '../views/content');
 
 console.log(`Searching for files in ${INPUT_DIR}`);
 
+const getContent = async (filePath) => {
+  const file = await fs.promises.readFile(filePath);
+  const content = file.toString();
+  return content.replace(/(^)(<!--|-->)/gm, '$1---');
+}
+const getPaths = (fileName) => {
+  const filePath = path.join(INPUT_DIR, '/', fileName);
+  const outputPath = path.join(OUTPUT_DIR, '/', fileName);
+  return {filePath, outputPath};
+}
+const createFile = async (parsedContent, outputPath) => {
+  await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
+  await fs.promises.writeFile(outputPath, parsedContent);
+}
+
 (async () => {
   await del(OUTPUT_DIR);
 
   glob(INPUT_GLOB, {cwd: INPUT_DIR}, async (err, files) => {
     await Promise.all(
       files.map(async (fileName) => {
-        const filePath = path.join(INPUT_DIR, '/', fileName);
-        const outputPath = path.join(OUTPUT_DIR, '/', fileName);
+        const {filePath, outputPath} = getPaths(fileName);
+        const parsedContent = await getContent(filePath);
 
-        const file = await fs.promises.readFile(filePath);
-        const content = file.toString();
-        const parsedContent = content.replace(/(^)(<!--|-->)/gm, '$1---');
-
-        await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
-        await fs.promises.writeFile(outputPath, parsedContent);
+        createFile(parsedContent, outputPath);
 
         console.log(`\t - File "${fileName}" processed`);
       })
@@ -35,36 +45,41 @@ console.log(`Searching for files in ${INPUT_DIR}`);
 if(process.argv.includes('watch')){
     const watch = chokidar.watch('../content');
 
-    watch.on('change', async fileInitPath => {
+    watch.on('change', async (fileInitPath) => {
       const fileName = fileInitPath.replace('\.\.\\content\\', '');
-      const filePath = path.join(INPUT_DIR, '/', fileInitPath);
-      const outputPath = path.join(OUTPUT_DIR, '/', fileName);
-      const file = await fs.promises.readFile(filePath);
-      const content = file.toString();
-      const parsedContent = content.replace(/(^)(<!--|-->)/gm, '$1---');
+      const {filePath, outputPath} = getPaths(fileName);
 
+      const parsedContent = await getContent(filePath);
       await fs.promises.writeFile(outputPath, parsedContent);
 
       console.log(`\t - ${fileName} - updated`);
     })
 
-    watch.on('add', async fileInitPath => {
+    watch.on('add', async (fileInitPath) => {
       const fileName = fileInitPath.replace('\.\.\\content\\', '');
-      const outputPath = path.join(OUTPUT_DIR, '/', fileName);
+      const {outputPath} = getPaths(fileName);
 
-      await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
-      await fs.promises.writeFile(outputPath, '');
+      createFile('', outputPath);
 
       console.log(`\t - ${fileName} - added`);
     })
 
-    watch.on('unlink', async fileInitPath => {
+    watch.on('unlink', async (fileInitPath) => {
       const fileName = fileInitPath.replace('\.\.\\content\\', '');
-      const outputPath = path.join(OUTPUT_DIR, '/', fileName);
+      const {outputPath} = getPaths(fileName);
 
       await fs.promises.unlink(outputPath);
 
       console.log(`\t - ${fileName} - deleted`);
+    })
+
+    watch.on('unlinkDir', async (dirInitPath) => {
+      const dirname = dirInitPath.replace('\.\.\\content\\', '');
+
+      const {outputPath} = getPaths(dirname);
+      await del(outputPath);
+
+      console.log(`\t - ${dirname} deleted`);
     })
 }
 
