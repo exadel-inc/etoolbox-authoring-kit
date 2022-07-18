7a199ba267878155c7cfc3283025935d0d9e3605
bhlynsky <86774452+bhlynsky@users.noreply.github.com>
2022-07-18 15:02:45 +0300

    [Tech] Added markdown plugin to change href attributes

--- docs/website/11ty/markdown.lib.js
+++ docs/website/11ty/markdown.lib.js
@@ -1,6 +1,7 @@
 const MarkdownIt = require('markdown-it');
 const MarkdownItAnchor = require('markdown-it-anchor');
 const {highlight} = require('./prismjs.lib');
+const mila = require("markdown-it-link-attributes");
 
 const markdown = MarkdownIt({html: true, highlight, linkify: true});
 
@@ -10,9 +11,21 @@ const slugify = (heading) => {
   heading = heading.replace(/@/g, '');
   return encodeURIComponent(heading);
 };
+
 markdown.use(MarkdownItAnchor, {level: 2, slugify, tabIndex: false});
+
+markdown.use(mila, {
+  matcher(href) {
+    return href.startsWith("https:");
+  },
+  attrs: {
+    target: '_blank',
+    rel: 'noopener noreferrer',
+  },
+});
 
 module.exports = (config) => {
   config.setLibrary('md', markdown);
 };
+
 module.exports.markdown = markdown;
