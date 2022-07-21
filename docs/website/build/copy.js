471cc5fe3778429df27c367ee33d7776b2f9c0aa
Bohdan Taradada <btaradada@exadel.com>
2022-07-21 11:17:08 +0300

    cosmetic fixes

--- docs/website/build/copy.js
+++ docs/website/build/copy.js
@@ -23,7 +23,7 @@ const getPaths = (fileName) => {
   const fileInitPath = fileName.replace('\.\.\\content\\', '');
   const inputPath = path.join(INPUT_DIR, '/', fileInitPath);
   const outputPath = path.join(OUTPUT_DIR, '/', fileInitPath);
-  return {inputPath, outputPath};
+  return { inputPath, outputPath };
 }
 
 const createFile = async (inputPath, outputPath, fileName) => {
@@ -31,7 +31,7 @@ const createFile = async (inputPath, outputPath, fileName) => {
   const exists = fs.existsSync(outputPath);
   await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
   await fs.promises.writeFile(outputPath, parsedContent);
-  console.log(`\t - ${fileName} - ${exists?'updated':'added'}`);
+  console.log(`\t - ${fileName} - ${exists ? 'updated' : 'added'}`);
 }
 
 const deleteFile = async (outputPath, fileName) => {
@@ -43,43 +43,43 @@ const deleteFile = async (outputPath, fileName) => {
 (async () => {
   await del(OUTPUT_DIR);
 
-  glob(INPUT_GLOB, {cwd: INPUT_DIR}, async (err, files) => {
+  glob(INPUT_GLOB, { cwd: INPUT_DIR }, async (err, files) => {
     await Promise.all(
       files.map(async (fileName) => {
-        const {inputPath, outputPath} = getPaths(fileName);
+        const { inputPath, outputPath } = getPaths(fileName);
         createFile(inputPath, outputPath, fileName);
       })
     );
   });
 })();
 
-if(process.argv.includes('watch')){
-    const watch = chokidar.watch('../content');
-    let pathsToUpdate = [];
+if (process.argv.includes('watch')) {
+  const watch = chokidar.watch('../content');
+  const pathsToUpdate = new Set();
+  let timer;
 
-    const updateData = async() => {
-      pathsToUpdate.forEach(async (path, idx) => {
-        const {inputPath, outputPath} = getPaths(path);
+  const updateData = async () => {
+    pathsToUpdate.forEach(async (path) => {
+      const { inputPath, outputPath } = getPaths(path);
 
-        const isDeleted = !fs.existsSync(inputPath);
+      const isDeleted = !fs.existsSync(inputPath);
 
-        if(isDeleted) {
-          await deleteFile(outputPath, path);
-        } else {
-          createFile(inputPath, outputPath, path);
-        }
+      if (isDeleted) {
+        await deleteFile(outputPath, path);
+      } else {
+        await createFile(inputPath, outputPath, path);
+      }
+    })
+    pathsToUpdate.clear();
+  }
 
-        if(pathsToUpdate.length - 1 === idx) pathsToUpdate = [];
-      })
-    }
-    const deferredsync = async (fileName) => {
-      !pathsToUpdate.includes(fileName) && pathsToUpdate.push(fileName)
-      let timer
-      clearTimeout(timer);
-      timer = setTimeout(()=>updateData(), DELAY);
-    }
+  const deferredsync = async (fileName) => {
+    pathsToUpdate.add(fileName);
+    clearTimeout(timer);
+    timer = setTimeout(() => updateData(), DELAY);
+  }
 
-    watch.on('change', deferredsync);
-    watch.on('add', deferredsync);
-    watch.on('unlink', deferredsync);
+  watch.on('change', deferredsync);
+  watch.on('add', deferredsync);
+  watch.on('unlink', deferredsync);
 }
