359d9d72ec1dd0f0a88067157ad71aaef1ba85b4
Stepan Miakchilo <smiakchilo@exadel.com>
2022-08-09 14:07:12 +0300

    [EAK-372] Fixed @AllowedChildren not working with mode MERGE on static templates

--- core/src/main/resources/policy-management/eakApplyTopLevelPolicy.js
+++ core/src/main/resources/policy-management/eakApplyTopLevelPolicy.js
@@ -2,10 +2,6 @@ window.eakApplyTopLevelPolicy = editable => {
     const rules = JSON.parse('%s').rules;
     const containers = rules.flatMap(rule => rule.containers);
     const resName = containers.find(container => editable.config.path.endsWith(container));
-    if (!resName) {
-        editable.config.eakIsRoot = false;
-        return;
-    }
 
     const filteredRules = rules.filter(rule => rule.containers).filter(rule => rule.containers.includes(resName));
     if (!filteredRules || !filteredRules.length) {
@@ -13,7 +9,6 @@ window.eakApplyTopLevelPolicy = editable => {
     }
     const listenerJson = {isEditConfig: true, rules: filteredRules};
     editable.config.editConfig.listeners.updatecomponentlist = Granite.PolicyResolver.build(JSON.stringify(listenerJson));
-    editable.config.eakIsRoot = true;
     editable.config.eakResourceName = resName;
-    editable.config.policyPath = 'fake-policies';
+
 };
