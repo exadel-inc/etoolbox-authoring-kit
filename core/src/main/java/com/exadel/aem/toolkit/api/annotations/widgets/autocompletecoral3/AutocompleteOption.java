4fac674fe40f9fdf6f0d3153f806e5976db81367
akaliada <aliaksei.kaliada@hp.com>
2022-08-28 16:54:44 +0200

    [EAK-366] Added test for Autocomplete component

--- core/src/main/java/com/exadel/aem/toolkit/api/annotations/widgets/autocompletecoral3/AutocompleteOption.java
+++ core/src/main/java/com/exadel/aem/toolkit/api/annotations/widgets/autocompletecoral3/AutocompleteOption.java
@@ -1,11 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.exadel.aem.toolkit.api.annotations.widgets.autocompletecoral3;
 
 import com.exadel.aem.toolkit.api.annotations.meta.PropertyRendering;
 
+/**
+ * Used to define an option within the {@link Autocomplete#options()} set
+ * See documentation on <a href="https://developer.adobe.com/experience-manager/reference-materials/6-5/coral-ui/coralui3/Coral.Autocomplete.html">
+ * Autocomplete</a> component
+ */
 public @interface AutocompleteOption {
 
+    /**
+     * Maps to the {@code text} attribute of this Coral UI component's node.
+     * Used to define the text displayed beside the option box
+     * @return String value
+     */
     String text();
 
+    /**
+     * Maps to the {@code value} attribute of this Coral UI component's node.
+     * Used to define the value to be stored when this option is checked
+     * @return String value
+     */
     @PropertyRendering(allowBlank = true)
     String value();
 }
--- core/src/main/java/com/exadel/aem/toolkit/core/authoring/models/Option.java
+++ core/src/main/java/com/exadel/aem/toolkit/core/authoring/models/Option.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.exadel.aem.toolkit.core.authoring.models;
 
 import org.apache.sling.api.resource.Resource;
@@ -5,6 +18,10 @@ import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+/**
+ * Represents the back-end part of the {@code AutocompleteOption} of the {@code Autocomplete} component for Granite UI dialogs. His Sling model
+ * is responsible for injecting and retrieving the properties of component's node.
+ */
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class Option {
 
@@ -14,10 +31,18 @@ public class Option {
     @ValueMapValue
     private String value;
 
+    /**
+     * Retrieves the specified text value of the autocomplete option
+     * @return String
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Retrieves the specified value of the autocomplete option
+     * @return String
+     */
     public String getValue() {
         return value;
     }
