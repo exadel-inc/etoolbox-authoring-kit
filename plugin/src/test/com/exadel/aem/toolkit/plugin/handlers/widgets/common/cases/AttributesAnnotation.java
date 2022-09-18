3eb4bd7670c89b8d6d00fc8abae9be1b8b9aedb6
Stepan Miakchilo <smiakchilo@exadel.com>
2022-09-18 21:45:40 +0300

    [Tech] Moved widgets meta settings-related test cases

--- plugin/src/test/com/exadel/aem/toolkit/plugin/handlers/widgets/common/cases/AttributesAnnotation.java
+++ plugin/src/test/com/exadel/aem/toolkit/plugin/handlers/widgets/common/cases/AttributesAnnotation.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.exadel.aem.toolkit.test.common;
+package com.exadel.aem.toolkit.plugin.handlers.widgets.common.cases;
 
 import com.exadel.aem.toolkit.api.annotations.main.AemComponent;
 import com.exadel.aem.toolkit.api.annotations.main.Dialog;
