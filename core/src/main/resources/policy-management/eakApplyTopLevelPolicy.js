96c811aee234ce0508761652fc9455fc7cf1d347
liubou-masiuk <54947899+liubou-masiuk@users.noreply.github.com>
2022-09-23 03:00:02 +0300

    [EAK-401] Fix issue with multiple root containers using the last applied root policy (#402)
    Co-authored-by: ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
    
    Co-authored-by: ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>

--- core/src/main/resources/policy-management/eakApplyTopLevelPolicy.js
+++ core/src/main/resources/policy-management/eakApplyTopLevelPolicy.js
@@ -1,14 +1,16 @@
 window.eakApplyTopLevelPolicy = editable => {
+    const {config} = editable;
     const rules = JSON.parse('%s').rules;
-    const containers = rules.flatMap(rule => rule.containers);
-    const resName = containers.find(container => editable.config.path.endsWith(container));
+    const containers = rules.flatMap((rule) => rule.containers);
+    const resName = containers.find((container) => config.path.endsWith(container));
 
-    const filteredRules = rules.filter(rule => rule.containers).filter(rule => rule.containers.includes(resName));
+    const filteredRules = rules.filter((rule) => (rule.containers || []).includes(resName));
     if (!filteredRules || !filteredRules.length) {
         return;
     }
     const listenerJson = {isEditConfig: true, rules: filteredRules};
-    editable.config.editConfig.listeners.updatecomponentlist = Granite.PolicyResolver.build(JSON.stringify(listenerJson));
-    editable.config.eakResourceName = resName;
+    const updatecomponentlist = Granite.PolicyResolver.build(JSON.stringify(listenerJson));
+    config.editConfig.listeners = Object.assign({}, config.editConfig.listeners, {updatecomponentlist});
+    config.eakResourceName = resName;
 
 };
