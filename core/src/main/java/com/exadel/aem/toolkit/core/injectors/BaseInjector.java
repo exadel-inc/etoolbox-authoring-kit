00340313f1915f41c6f6981a2ad87cda8c62e29e
mykhailivchenko <mivchenko@exadel.com>
2022-10-03 15:12:45 +0300

    [EAK-397] ValueMapEnumValueInjector: add support for default values

--- core/src/main/java/com/exadel/aem/toolkit/core/injectors/BaseInjector.java
+++ core/src/main/java/com/exadel/aem/toolkit/core/injectors/BaseInjector.java
@@ -1,8 +1,12 @@
 package com.exadel.aem.toolkit.core.injectors;
 
-import static com.exadel.aem.toolkit.core.injectors.InjectorConstants.EXCEPTION;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Type;
+import java.util.Objects;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
@@ -11,12 +15,7 @@ import org.apache.sling.models.spi.Injector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.CheckForNull;
-import javax.annotation.Nonnull;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
-import java.lang.reflect.Type;
-import java.util.Objects;
+import static com.exadel.aem.toolkit.core.injectors.InjectorConstants.EXCEPTION;
 
 /**
  * The parent injector class, which is an implementation of the design pattern - template method. The class accumulates an abstract algorithm for injecting a value into an annotated field.
@@ -68,6 +67,24 @@ public abstract class BaseInjector<AnnotationType extends Annotation> implements
         return value;
     }
 
+    private boolean getHasDefaultValue(AnnotatedElement element) {
+        return element.isAnnotationPresent(Default.class);
+    }
+
+    /**
+     * The method assumes getting a default value based on the Default annotation.
+     * Can be useful in cases where the functionality of the standard default injection algorithm is not enough.
+     * {@see org.apache.sling.models.impl.model.AbstractInjectableElement}
+     * @param element           {@link AnnotatedElement} instance that facades the Java class member allowing to retrieve
+     *                          annotation objects
+     * @param type             Type of receiving Java class member
+     * @return Object wrapped value
+     */
+    Object getDefaultValue(AnnotatedElement element, Type type) {
+        //implement me if needed
+        return null;
+    }
+
     /**
      * Get the annotation class based on elements declared annotation
      * The necessary implementation is needed to implement in the descendant classes
@@ -82,7 +99,7 @@ public abstract class BaseInjector<AnnotationType extends Annotation> implements
      * @param adaptable    A {@link SlingHttpServletRequest} or a {@link Resource} instance
      * @param name         Name of the Java class member to inject the value into
      * @param type         Type of receiving Java class member
-     * @param defaultValue
+     * @param defaultValue {@see BaseInjector.getDefaultValue}
      * @return {@code Resource} or adapted object if successful. Otherwise, null is returned
      */
     abstract Object getValue(
@@ -94,85 +111,9 @@ public abstract class BaseInjector<AnnotationType extends Annotation> implements
 
     /**
      * Generates and displays an error message if the value cannot be injected.
-     * @param annotation Аннотация, объявленная в целевом поле
+     * @param annotation target annotation
      */
     void logError(AnnotationType annotation) {
         LOG.debug(EXCEPTION, annotation, getName());
     }
-
-    static boolean getHasDefaultValue(AnnotatedElement element) {
-        return element.isAnnotationPresent(Default.class);
-    }
-
-    static Object getDefaultValue(AnnotatedElement element, Type type) {
-        Default defaultAnnotation = element.getAnnotation(Default.class);
-        if (defaultAnnotation == null) {
-            return null;
-        }
-
-        Object value = null;
-
-        if (type instanceof Class) {
-            Class<?> injectedClass = (Class<?>) type;
-            if (injectedClass.isArray()) {
-                Class<?> componentType = injectedClass.getComponentType();
-                if (componentType == String.class) {
-                    value = defaultAnnotation.values();
-                } else if (componentType == Integer.TYPE) {
-                    value = defaultAnnotation.intValues();
-                } else if (componentType == Integer.class) {
-                    value = ArrayUtils.toObject(defaultAnnotation.intValues());
-                } else if (componentType == Long.TYPE) {
-                    value = defaultAnnotation.longValues();
-                } else if (componentType == Long.class) {
-                    value = ArrayUtils.toObject(defaultAnnotation.longValues());
-                } else if (componentType == Boolean.TYPE) {
-                    value = defaultAnnotation.booleanValues();
-                } else if (componentType == Boolean.class) {
-                    value = ArrayUtils.toObject(defaultAnnotation.booleanValues());
-                } else if (componentType == Short.TYPE) {
-                    value = defaultAnnotation.shortValues();
-                } else if (componentType == Short.class) {
-                    value = ArrayUtils.toObject(defaultAnnotation.shortValues());
-                } else if (componentType == Float.TYPE) {
-                    value = defaultAnnotation.floatValues();
-                } else if (componentType == Float.class) {
-                    value = ArrayUtils.toObject(defaultAnnotation.floatValues());
-                } else if (componentType == Double.TYPE) {
-                    value = defaultAnnotation.doubleValues();
-                } else if (componentType == Double.class) {
-                    value = ArrayUtils.toObject(defaultAnnotation.doubleValues());
-                } else if (((Class<?>) type).getComponentType() != null && ((Class<?>) type).getComponentType().isEnum()) {
-                    value = defaultAnnotation.values();
-                } else {
-                    LOG.warn("Default values for {} are not supported", componentType);
-                }
-            } else {
-                if (injectedClass == String.class) {
-                    value = defaultAnnotation.values().length == 0 ? "" : defaultAnnotation.values()[0];
-                } else if (injectedClass == Integer.class) {
-                    value = defaultAnnotation.intValues().length == 0 ? 0 : defaultAnnotation.intValues()[0];
-                } else if (injectedClass == Long.class) {
-                    value = defaultAnnotation.longValues().length == 0 ? 0l : defaultAnnotation.longValues()[0];
-                } else if (injectedClass == Boolean.class) {
-                    value = defaultAnnotation.booleanValues().length == 0 ? false : defaultAnnotation.booleanValues()[0];
-                } else if (injectedClass == Short.class) {
-                    value = defaultAnnotation.shortValues().length == 0 ? ((short) 0) : defaultAnnotation.shortValues()[0];
-                } else if (injectedClass == Float.class) {
-                    value = defaultAnnotation.floatValues().length == 0 ? 0f : defaultAnnotation.floatValues()[0];
-                } else if (injectedClass == Double.class) {
-                    value = defaultAnnotation.doubleValues().length == 0 ? 0d : defaultAnnotation.doubleValues()[0];
-                } else if (injectedClass.isEnum()) {
-                    value = defaultAnnotation.values();
-                }
-                else {
-                    LOG.warn("Default values for {} are not supported", injectedClass);
-                }
-            }
-        } else {
-            LOG.warn("Cannot provide default for {}", type);
-        }
-        return value;
-    }
-
 }
--- core/src/main/java/com/exadel/aem/toolkit/core/injectors/ValueMapEnumValueInjector.java
+++ core/src/main/java/com/exadel/aem/toolkit/core/injectors/ValueMapEnumValueInjector.java
@@ -11,6 +11,7 @@ import com.exadel.aem.toolkit.core.injectors.utils.TypeUtil;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.wrappers.DeepReadValueMapDecorator;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.spi.Injector;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
@@ -28,17 +29,38 @@ public class ValueMapEnumValueInjector extends BaseInjector<EnumValue> {
 
     public static final String NAME = "eak-etoolbox-enum-injector";
 
+    /**
+     * {@inheritDoc}
+     */
     @Nonnull
     @Override
     public String getName() {
         return NAME;
     }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     EnumValue getAnnotation(AnnotatedElement element) {
         return element.getDeclaredAnnotation(EnumValue.class);
     }
 
-    @SuppressWarnings("uncheked")
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    Object getDefaultValue(AnnotatedElement element, Type type) {
+        Default defaultAnnotation = element.getAnnotation(Default.class);
+        if (defaultAnnotation != null && isValid(type)) {
+            return defaultAnnotation.values();
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     Object getValue(
         Object adaptable,
@@ -76,17 +98,30 @@ public class ValueMapEnumValueInjector extends BaseInjector<EnumValue> {
 
         return anEnum;
     }
+
+    /**
+     * Checks the {@code type} is an enumeration. If the {@code type} is a collection, the elements of the array/collection are checked.
+     * @param type Type of receiving Java class member
+     * @return true/false
+     */
     private boolean isValid(Type type) {
         return (type instanceof Class && ((Class<?>) type).isEnum()) || TypeUtil.isValidArray(type) || TypeUtil.isValidCollection(type);
     }
 
+    /**
+     * Attempts to get value of an enum java class member {@code type}
+     * and a string representation of a constant or a list of constants @{code stringValue}
+     * @param type        Type of receiving Java class member
+     * @param stringValue String value(s) of enum constant(s)
+     * @return enum constant or Array/List of enum constants
+     */
     private Object getEnum(Class<?> type, String[] stringValue) {
         if (stringValue == null) {
             return null;
         }
 
         Class<?> componentType = TypeUtil.extractComponentType(type);
-        if(componentType == null) {
+        if (componentType == null) {
             return null;
         }
 
@@ -94,7 +129,9 @@ public class ValueMapEnumValueInjector extends BaseInjector<EnumValue> {
             return getSingle(componentType, String.valueOf(stringValue[0]));
         } else {
             List<Object> objects = Arrays.asList(stringValue);
-            Object[] array = Arrays.stream(componentType.getEnumConstants()).filter(e -> objects.contains(String.valueOf(e))).toArray();
+            Object[] array = Arrays.stream(componentType.getEnumConstants())
+                .filter(e -> objects.contains(String.valueOf(e)))
+                .toArray();
             Object[] enumTypeArray = (Object[]) TypeUtil.transformArray(array, componentType);
 
             if (type.isArray()) {
@@ -105,14 +142,24 @@ public class ValueMapEnumValueInjector extends BaseInjector<EnumValue> {
         }
     }
 
-    private Object getEnumByParam(Type type, String paramName, String[] stringValue) {
-        if(stringValue == null) {
+    /**
+     * Attempts to get the value of an enum java class member based on the enum value field
+     * and a string representation of a constant or a list of constants @{code stringValue}
+     * @param type        Type of receiving Java class member
+     * @param paramName   name of the field that represents the enum value
+     * @param stringValue String value of enum constant
+     * @return enum constant
+     */
+    private Object getEnumByParam(Class<?> type, String paramName, String[] stringValue) {
+        if (stringValue == null) {
             return null;
         }
 
-        Field param = Arrays.stream(((Class<?>) type).getDeclaredFields())
+        Field param = Arrays.stream(type.getDeclaredFields())
             .filter(x -> !x.isEnumConstant() && !x.isSynthetic())
-            .filter(x -> StringUtils.equals(paramName, x.getName())).findFirst().orElse(null);
+            .filter(x -> StringUtils.equals(paramName, x.getName()))
+            .findFirst()
+            .orElse(null);
 
         if (param == null) {
             return null;
@@ -122,7 +169,7 @@ public class ValueMapEnumValueInjector extends BaseInjector<EnumValue> {
             param.setAccessible(true);
         }
 
-        for (Object enumConstant : ((Class<?>) type).getEnumConstants()) {
+        for (Object enumConstant : type.getEnumConstants()) {
             try {
                 Object constant = param.get(enumConstant);
                 if (StringUtils.equals(stringValue[0], String.valueOf(constant))) {
@@ -136,6 +183,12 @@ public class ValueMapEnumValueInjector extends BaseInjector<EnumValue> {
         return null;
     }
 
+    /**
+     * Retrieve the first matching enum value of a constant with a string representation of the value
+     * @param type        Type of receiving Java class member
+     * @param stringValue String value of enum constant
+     * @return enum constant
+     */
     private Object getSingle(Class<?> type, String stringValue) {
         return Arrays.stream(type.getEnumConstants())
             .filter(e -> StringUtils.equals(stringValue, String.valueOf(e)))
