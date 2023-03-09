d32aaf7e018ca61da1dfc686d1cafa71a0584554
Stepan Miakchilo <smiakchilo@exadel.com>
2023-03-09 14:19:47 +0200

    [Tech] Added support for TextArea, ImageUpload (coral-picker) widgets

---
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-authoring-kit/components/authoring/aiBridge/js/image/imageUploadPlugin.js
@@ -0,0 +1,89 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function ($, ns) {
+    'use strict';
+
+    const SELECTOR = 'coral-fileupload[data-eak-plugins*="assistant"]';
+    const CLS_PROMPT = 'eak-assistant-prompt';
+
+    $(document).on('coral-overlay:open', function (e) {
+        const $dialog = $(e.target);
+        $dialog.find(SELECTOR).each(async function () {
+            const $imageUpload = $(this);
+            const imageUploadName = $imageUpload.attr('name');
+            $imageUpload.attr(ns.Assistant.ATTR_TARGET, true);
+            $imageUpload.get(0).acceptDelegate = async function (value) {
+                const retrievedImage = await retrieveImage($imageUpload, value);
+                const assetSelectedEvent = $.Event('assetselected');
+                assetSelectedEvent.path = retrievedImage.path;
+                assetSelectedEvent.mimetype = retrievedImage.type;
+                assetSelectedEvent.thumbnail = $(`<img src="${retrievedImage.path}?cq_ck=${Date.now()}" alt="${retrievedImage.path}">`);
+                $imageUpload.trigger(assetSelectedEvent);
+            }
+
+            const $innerWrapper = $imageUpload.wrap(`<div class="eak-assistant-prompt-wrapper align-bottom"></div>`).parent();
+
+            const textField = new Coral.Textfield();
+            textField.set({
+                placeholder: 'Image description to use with the generator service',
+                value: ns.Assistant.getSettings(imageUploadName)['prompt'] || ''
+            });
+            const $textField = $(textField)
+                .addClass(`${CLS_PROMPT} coral-Form-field`)
+                .attr(ns.Assistant.ATTR_SOURCE, true)
+                .attr(ns.Assistant.ATTR_SOURCE_SETTING_REF, `${imageUploadName}@prompt`);
+            $innerWrapper.append($textField);
+            await ns.Assistant.initAssistantUi($innerWrapper, { facilityPrefix: 'image.' });
+        });
+    });
+
+    async function retrieveImage($imageUpload, src) {
+        const action = $imageUpload.attr('action');
+        if (!action) {
+            return;
+        }
+
+        if (!/^\w+:\/\//.test(src)) {
+            return Promise.resolve({ path: src, type: 'image' });
+        }
+
+        const fieldName = ($imageUpload.attr('name') || '').replace(/(^\.\/|\/+$)/g, '') || 'image';
+        const importToSubfolder = needsImportToSubfolder($imageUpload);
+        const target = `${action}/${fieldName}${importToSubfolder ? '/imported' : ''}`;
+        const serviceLink = ns.Assistant.getServiceLink({
+            command: 'image.util.import',
+            settings: {
+                from: src,
+                to: target
+            }
+        })
+        return new Promise((resolve, reject) => {
+            fetch(serviceLink, {method: 'POST'})
+                .then((response) => response.json())
+                .then((json) => resolve({ path: json.path, type: json.type }))
+                .catch((e) => reject(e));
+
+        })
+    }
+
+    function needsImportToSubfolder($imageUpload) {
+        const fieldName = $imageUpload.attr('name');
+        const fileRefName = $imageUpload.find('[data-cq-fileupload-parameter="filereference"]').attr('name');
+        if (!fieldName || !fileRefName) {
+            return false;
+        }
+        return fileRefName.startsWith(fieldName);
+    }
+
+})(Granite.$, window.eak = window.eak || {});
--- ui.apps/src/main/content/jcr_root/apps/etoolbox-authoring-kit/components/authoring/aiBridge/js/text/textPlugin.js
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-authoring-kit/components/authoring/aiBridge/js/text/textPlugin.js
@@ -14,93 +14,14 @@
 (function ($, ns) {
     'use strict';
 
-    const WRAPPER_SELECTOR = 'eak-Form-field-wrapper';
-    const INPUT_SELECTOR = '[data-eak-plugins*="ai"]';
-    const BUTTON_SELECTOR = '[is="coral-anchorbutton"]';
+    const SELECTOR = '.coral3-Textfield[data-eak-plugins*="assistant"], textarea[data-eak-plugins*="assistant"]';
 
     $(document).on('coral-overlay:open', function (e) {
         const $dialog = $(e.target);
-        $dialog.find(INPUT_SELECTOR).each(async function () {
+        $dialog.find(SELECTOR).each(async function () {
             const $input = $(this);
-            const buttonId = 'input-' + $input.attr('name').replace(/^[^\w]+|[^\w]+$/, '').replace(':', '-');
-            const menuOptions = await ns.Ai.getMenuOptions();
-            const menuContent = '<coral-buttonlist>' +
-                menuOptions
-                    .map(option => {
-                        const variants = option.variants
-                            ? option.variants.map(v => { return { title: v.vendor, id: v.id }; })
-                            : [{ title: option.vendor, id: option.id }];
-                        const variantsString = JSON.stringify(variants);
-                        const paramsString = option.params
-                            ? JSON.stringify(option.params).replace(/"/g, '&quot;')
-                            : '';
-                        return `<button is="coral-buttonlist-item" icon="${option.icon}" data-variants=${variantsString} data-params="${paramsString}" value="${option.id}">${option.title}</button>`;
-                    })
-                    .join('') +
-                '</coral-buttonlist>';
-
-            const $wrapper = $input.wrap(`<div class="${WRAPPER_SELECTOR}"></div>`).parent();
-            const $button = $(`<a is="coral-anchorbutton" id="${buttonId}" icon="ai" iconsize="S"></a>`);
-            if (!$input.val()) {
-                $button.attr('disabled', true);
-            }
-            $button.appendTo($wrapper);
-
-            const $popover = $(`<coral-popover placement="bottom" target="#${buttonId}"><coral-popover-content>${menuContent}</coral-popover-content></coral-popover>`).appendTo($wrapper);
-            $popover.on('click', 'button', onMenuItemClick);
+            $input.attr(ns.Assistant.ATTR_SOURCE, true);
+            await ns.Assistant.initAssistantUi($input, { facilityPrefix: 'text.' });
         });
-        $dialog.on('keydown', INPUT_SELECTOR, $.debounce(500, onTextChange));
     });
-
-    async function onMenuItemClick(e) {
-        e.preventDefault();
-        const $this = $(this);
-        $this.closest('coral-popover').hide();
-        const command = $this.attr('value');
-        if (command === 'settings') {
-            return ns.Ai.openSettingsDialog();
-        }
-
-        const $input = $this.closest('.' + WRAPPER_SELECTOR).find('input');
-        const dialogSetup = {
-            payload: getText($input),
-            variants: $this.data('variants'),
-            params: $this.data('params'),
-            acceptDelegate: function (text) {
-                setText($input.attr('name'), text);
-            }
-        };
-        ns.Ai.openRequestDialog(dialogSetup);
-    }
-
-    function getText($input) {
-        const start = $input[0].selectionStart;
-        const end = $input[0].selectionEnd;
-        if (end > start) {
-            return $input.val().substring(start, end);
-        }
-        return $input.val();
-    }
-
-    function setText(name, text) {
-        const $input = $(document).find(`[name="${name}"]`);
-        const start = $input[0].selectionStart;
-        const end = $input[0].selectionEnd;
-        if (end > start) {
-            const newText = $input.val().substring(0, start) + text + $input.val().substring(end);
-            $input.val(newText);
-        } else {
-            $input.val(text);
-        }
-    }
-
-    function onTextChange(e) {
-        const $this = $(e.target);
-        const $button = $this.closest('.' + WRAPPER_SELECTOR).find(BUTTON_SELECTOR);
-        if ($this.val()) {
-            $button.removeAttr('disabled');
-        } else {
-            $button.attr('disabled', true);
-        }
-    }
 })(Granite.$, window.eak = window.eak || {});
