6b045e0cbe8a0a5c0b58d264b3ae4a69a0d4a6a1
Stepan Miakchilo <smiakchilo@exadel.com>
2023-04-04 00:10:38 +0300

    [EAK-433] Updated Javadoc

--- core/src/main/java/com/exadel/aem/toolkit/api/annotations/injectors/Children.java
+++ core/src/main/java/com/exadel/aem/toolkit/api/annotations/injectors/Children.java
@@ -29,14 +29,20 @@ import com.exadel.aem.toolkit.core.injectors.filters.NonGhostFilter;
 import com.exadel.aem.toolkit.core.injectors.filters.NonNullFilter;
 
 /**
- * Used on a field, a method, or a method parameter of a Sling model to inject a collection of either secondary models
- * derived from the child (relative) resources or the resources themselves. One can select particular properties used
- * for injection by specifying their common prefix and/or postfix, or else provide a filtering {@link Predicate}.
- * <p>Unlike in {@link Child}, one does not specify the precise Sling object to inject but instead the "parent" object
- * children of which are injected.
- * <p>The type of the underlying Java array (parameter type of Java collection) must be {@link
- * org.apache.sling.api.resource.Resource} or a class that is adaptable from {@code Resource}. Otherwise, nothing is
- * injected
+ * Used on a field, a method, or a method parameter of a Sling model to inject one of the following: an array or a
+ * collection of child/relative resources of the current one; or an array/collection of other Sling models based on the
+ * previous. Use "{@code ..}" to traverse to a parent resource. Use "{@code ./}" to refer to the current resource
+ * itself.
+ * <p>Note: unlike in {@link Child}, one does not specify the precise Sling object to inject but instead
+ * the "parent" object * children of which are injected.</p>
+ * <p>You can choose to use only a subset of resource properties used for injection by specifying their common
+ * {@code prefix} and/or {@code postfix}.</p>
+ * <p>Also you can provide one or more references to classes serving as filters for the children/related resources.
+ * These classes must implement {@link Predicate}. (See, e.g., {@link NonNullFilter}).
+ * <p>The underlying Java class member must be or an array type or else {@link java.util.Collection},
+ * {@link java.util.List} or {@link java.util.Set}. A member of array/collection must be of type
+ * {@link org.apache.sling.api.resource.Resource} or else a class that is adaptable from {@code Resource}. Otherwise,
+ * nothing is injected
  * @see Child
  */
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
@@ -62,14 +68,14 @@ public @interface Children {
     String prefix() default StringUtils.EMPTY;
 
     /**
-     * Used to specify the prefix. If set to a non-blank string, the properties of the child (relative) resource that
-     * end with the given value will be used for injection, while others will be skipped
+     * Used to specify the postfix. If set to a non-blank string, only the properties of the child (relative) resource
+     * having names ending with the given value will be used for injection, while others will be skipped
      * @return Optional non-blank string
      */
     String postfix() default StringUtils.EMPTY;
 
     /**
-     * Used to specify filters for the children The filters are a sequence of {@code Predicate<Resource>} instances
+     * Used to specify filters for the children. The filters are one or more {@code Predicate<Resource>} instances
      * referred to with their class names. Resources will be probed against these predicates. One possible predicate is
      * "this resource is not a ghost component", or else "this resource is not null"
      * @return Optional array of predicates
