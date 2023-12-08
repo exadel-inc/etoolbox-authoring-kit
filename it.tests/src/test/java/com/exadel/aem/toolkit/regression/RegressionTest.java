5fe6b8db4a1610119661749fb7f77d0e97f3cabc
Stepan Miakchilo <smiakchilo@exadel.com>
2023-12-08 15:31:47 +0300

    [EAK-490] Implemented fallback behavior

--- it.tests/src/test/java/com/exadel/aem/toolkit/regression/RegressionTest.java
+++ it.tests/src/test/java/com/exadel/aem/toolkit/regression/RegressionTest.java
@@ -13,6 +13,7 @@
  */
 package com.exadel.aem.toolkit.regression;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,165 +34,181 @@ public class RegressionTest {
 
     static final Logger LOG = LoggerFactory.getLogger(RegressionTest.class);
 
-    private static final String SHELL_COMMAND_LOG_FORMAT = "{}> {}";
-
-    private static final String[] ALLOWED_PACKAGING_TYPES = {"pom", "jar", "maven-plugin"};
+    private static final String SHELL_COMMAND_FORMAT = "{}> {}";
+    private static final String OPERATION_FAILED = "%s failed (wrong exit code)";
 
     private static final Pattern PATTERN_LOG_LEVEL = Pattern.compile("^\\s*\\[([A-Z]+)]\\s+");
 
-    private ProjectSettings settings;
+    private RegressionSettings settings;
     private RegressionAssetHelper assetHelper;
 
     @Before
     public void setUp() {
-        settings = new ProjectSettings();
+        settings = new RegressionSettings();
         assetHelper = new RegressionAssetHelper();
     }
 
     @Test
     public void doRegression() {
         settings.validate();
-
-        boolean shouldReplaceSnapshot = StringUtils.equals(settings.getEakVersion(), settings.getProjectVersion())
-            && StringUtils.contains(settings.getEakVersion(), "-SNAPSHOT");
-
-        boolean result = shouldReplaceSnapshot ? doRegressionWithEqualVersions() : doRegressionWithDifferingVersions();
-        if (settings.cleanUp()) {
-            LOG.info("Cleaning up regression content");
-            assetHelper.cleanUp();
-        } else {
-            LOG.info("Regression content is stored at {}", assetHelper.getTempRoot());
+        for (ProjectInfo project : settings.getProjects()) {
+            project.validate();
+            doRegression(project);
         }
-        Assert.assertTrue("There were differences in regression content. See log above", result);
     }
 
-    private boolean doRegressionWithDifferingVersions() {
-        Commandline commandLine = ShellHelper.getBuildCommand(settings);
+    private void doRegression(ProjectInfo project) {
+        LOG.info("Using project {}", project.getDirectory());
+        LOG.info("Comparing version {} to {}", project.getDeclaredEakVersion(), settings.getCurrentEakVersion());
+        LOG.info("Using Maven executable {}", settings.getMavenExecutable());
 
-        LOG.info("Running regression build with EAK version {}", settings.getProjectVersion());
-        LOG.info(SHELL_COMMAND_LOG_FORMAT, settings.getProjectDirectory(), commandLine);
-        int exitCode = runCommand(commandLine);
-        Assert.assertEquals("Regression build with EAK version " + settings.getProjectVersion() + " failed with code " + exitCode,
+        try {
+            boolean versionsAreEqual = StringUtils.equals(project.getDeclaredEakVersion(), settings.getCurrentEakVersion())
+                && StringUtils.contains(settings.getCurrentEakVersion(), "-SNAPSHOT");
+            boolean result = !versionsAreEqual
+                ? doRegressionWithDifferingVersions(project)
+                : doRegressionWithEqualVersions(project);
+            Assert.assertTrue("There were differences in regression content. See log above", result);
+        } finally {
+            if (settings.cleanUp()) {
+                LOG.info("Cleaning up regression content");
+                assetHelper.cleanUp();
+            } else {
+                LOG.info("Regression content is stored at {}", assetHelper.getTempRoot());
+            }
+        }
+    }
+
+    private boolean doRegressionWithDifferingVersions(ProjectInfo project) {
+        LOG.info("Running regression build with EAK version {}", project.getDeclaredEakVersion());
+
+        Commandline buildCommand = ShellHelper.getBuildCommand(settings, project, project.getDeclaredEakVersion());
+        LOG.info(SHELL_COMMAND_FORMAT, buildCommand.getWorkingDirectory(), buildCommand);
+        int exitCode = runCommand(buildCommand);
+        Assert.assertEquals(
+            String.format(OPERATION_FAILED, "Regression build with EAK version " + project.getDeclaredEakVersion()),
             0,
             exitCode);
 
-        Path oldPackagesDirectory = assetHelper.retainPackages(settings.getProjectDirectory(), "old");
-        LOG.info("EAK version {} files stored at {}", settings.getProjectVersion(), oldPackagesDirectory);
+        Path oldPackagesDirectory = assetHelper.retainPackages(project.getDirectory(), "old");
+        LOG.info("EAK version {} files stored at {}", project.getDeclaredEakVersion(), oldPackagesDirectory);
 
-        LOG.info("Running regression build with EAK version {}", settings.getEakVersion());
-        commandLine = ShellHelper.getBuildCommand(settings);
-        commandLine.addArguments(new String[] {
-            String.format(ShellHelper.MAVEN_PROP_FORMAT, settings.getVersionProperty(), settings.getEakVersion())
-        });
-        LOG.info(SHELL_COMMAND_LOG_FORMAT, settings.getProjectDirectory(), commandLine);
-        exitCode = runCommand(commandLine);
-        Assert.assertEquals("Regression build with EAK version " + settings.getEakVersion() + " failed with code " + exitCode,
+        LOG.info("Running regression build with EAK version {}", settings.getCurrentEakVersion());
+
+        buildCommand = ShellHelper.getBuildCommand(settings, project, settings.getCurrentEakVersion());
+        LOG.info(SHELL_COMMAND_FORMAT, buildCommand.getWorkingDirectory(), buildCommand);
+        exitCode = runCommand(buildCommand);
+        Assert.assertEquals(
+            String.format(OPERATION_FAILED, "Regression build with EAK version " + settings.getCurrentEakVersion()),
             0,
             exitCode);
 
         Path newPackagesDirectory = assetHelper.retainPackages(
-            settings.getProjectDirectory(),
+            project.getDirectory(),
             oldPackagesDirectory.getParent().resolve("new"));
-        LOG.info("EAK version {} files stored at {}", settings.getEakVersion(), newPackagesDirectory);
+        LOG.info("EAK version {} files stored at {}", settings.getCurrentEakVersion(), newPackagesDirectory);
 
-        return ComparisonUtil.isMatch(oldPackagesDirectory, newPackagesDirectory, settings.getFilters());
+        return ComparisonUtil.isMatch(oldPackagesDirectory, newPackagesDirectory, settings.getFilters(project));
     }
 
-    private boolean doRegressionWithEqualVersions() {
-        int exitCode;
-
+    private boolean doRegressionWithEqualVersions(ProjectInfo project) {
         Path repoDirectory = assetHelper.createTempRoot("repo");
+        String fakeVersion = settings.getCurrentEakVersion().replace("-SNAPSHOT", ".old-SNAPSHOT");
 
-        LOG.info("Retrieving former EAK assembly of version {}", settings.getEakVersion());
+        LOG.info("Retrieving former EAK assembly of version {}", settings.getCurrentEakVersion());
         List<ArtifactInfo> activeArtifacts = getInstallableArtifacts(repoDirectory);
         Assert.assertFalse("Could not download any artifacts", activeArtifacts.isEmpty());
 
         LOG.info("Installing former EAK assembly files to local repository");
-        installArtifacts(activeArtifacts, repoDirectory);
+        installArtifacts(activeArtifacts, repoDirectory, fakeVersion);
 
-        LOG.info("Running regression build with the former EAK assembly");
-        Commandline buildCommandLine = ShellHelper.getBuildCommand(settings);
-        buildCommandLine.addArguments(new String[] {
-            String.format(ShellHelper.MAVEN_PROP_FORMAT, settings.getVersionProperty(), ShellHelper.MAVEN_SNAPSHOT_VERSION)
-        });
-        LOG.info(SHELL_COMMAND_LOG_FORMAT, settings.getProjectDirectory(), buildCommandLine);
-        exitCode = runCommand(buildCommandLine);
-        Assert.assertEquals(
-            "Regression build with the former EAK assembly failed with code " + exitCode,
-            0,
-            exitCode);
-        Path oldPackagesDirectory = assetHelper.retainPackages(settings.getProjectDirectory(), "old");
-        LOG.info("Former EAK assembly package files stored at {}", oldPackagesDirectory);
+        try {
+            LOG.info("Running regression build with the former EAK assembly");
 
-        LOG.info("Running regression build with the current EAK assembly");
-        buildCommandLine = ShellHelper.getBuildCommand(settings);
-        buildCommandLine.addArguments(new String[] {
-            String.format(ShellHelper.MAVEN_PROP_FORMAT, settings.getVersionProperty(), settings.getEakVersion())
-        });
-        LOG.info(SHELL_COMMAND_LOG_FORMAT, settings.getProjectDirectory(), buildCommandLine);
-        exitCode = runCommand(buildCommandLine);
-        Assert.assertEquals(
-            "Regression build with the current EAK assembly exited with code " + exitCode,
-            0,
-            exitCode);
-        Path newPackagesDirectory = assetHelper.retainPackages(
-            settings.getProjectDirectory(),
-            oldPackagesDirectory.getParent().resolve("new"));
-        LOG.info("Current EAK assembly package files stored at {}", newPackagesDirectory);
+            Commandline buildCommand = ShellHelper.getBuildCommand(settings, project, fakeVersion);
+            LOG.info(SHELL_COMMAND_FORMAT, buildCommand.getWorkingDirectory(), buildCommand);
+            int exitCode = runCommand(buildCommand);
+            Assert.assertEquals(
+                String.format(OPERATION_FAILED, "Regression build with the former EAK assembly"),
+                0,
+                exitCode);
 
-        boolean result = ComparisonUtil.isMatch(oldPackagesDirectory, newPackagesDirectory, settings.getFilters());
-        if (settings.cleanUp()) {
+            Path oldPackagesDirectory = assetHelper.retainPackages(project.getDirectory(), "old");
+            LOG.info("Former EAK assembly package files stored at {}", oldPackagesDirectory);
+
+            LOG.info("Running regression build with the current EAK assembly");
+
+            buildCommand = ShellHelper.getBuildCommand(settings, project, settings.getCurrentEakVersion());
+            LOG.info(SHELL_COMMAND_FORMAT, buildCommand.getWorkingDirectory(), buildCommand);
+            exitCode = runCommand(buildCommand);
+            Assert.assertEquals(
+                String.format(OPERATION_FAILED, "Regression build with the current EAK assembly"),
+                0,
+                exitCode);
+
+            Path newPackagesDirectory = assetHelper.retainPackages(
+                project.getDirectory(),
+                oldPackagesDirectory.getParent().resolve("new"));
+            LOG.info("Current EAK assembly package files stored at {}", newPackagesDirectory);
+
+            return ComparisonUtil.isMatch(oldPackagesDirectory, newPackagesDirectory, settings.getFilters(project));
+
+        } finally {
             LOG.info("Purging former EAK assembly from the local repository");
-            uninstallArtifacts(activeArtifacts, repoDirectory);
+            uninstallArtifacts(activeArtifacts);
         }
-        return result;
     }
 
     private List<ArtifactInfo> getInstallableArtifacts(Path repoDirectory) {
         List<ArtifactInfo> result = new ArrayList<>();
         for (ArtifactInfo artifact : settings.getEakArtifacts()) {
-            if (!StringUtils.equalsAny(artifact.getPackaging(), ALLOWED_PACKAGING_TYPES)
-                || artifact.getId().contains("tests")) {
+            if (!artifact.isDownloadable()) {
                 continue;
             }
-            String artifactId = artifact.toString();
-            if ("pom".equals(artifact.getPackaging())) {
-                artifactId += ":pom";
+            Commandline downloadCommand = ShellHelper.getDownloadCommand(
+                settings,
+                artifact.getFullName(artifact.getFileExtension()),
+                repoDirectory);
+            LOG.info(SHELL_COMMAND_FORMAT, downloadCommand.getWorkingDirectory(), downloadCommand);
+            int exitCode = runCommand(downloadCommand);
+            if (!ArtifactInfo.PACKAGING_POM.equals(artifact.getPackaging())) {
+                Commandline downloadPomCommand = ShellHelper.getDownloadCommand(
+                    settings,
+                    artifact.getFullName(ArtifactInfo.PACKAGING_POM),
+                    repoDirectory);
+                LOG.info(SHELL_COMMAND_FORMAT, downloadPomCommand.getWorkingDirectory(), downloadCommand);
+                exitCode += runCommand(downloadPomCommand);
             }
-            Commandline downloadCommandLine = ShellHelper.getDownloadCommand(settings, artifactId, repoDirectory);
-            LOG.info(SHELL_COMMAND_LOG_FORMAT, repoDirectory, downloadCommandLine);
-            int exitCode = runCommand(downloadCommandLine);
             if (exitCode == 0) {
                 artifact.resolveFrom(repoDirectory);
             }
-            if (artifact.getPath() != null) {
+            if (artifact.getPath() != null && artifact.getPomPath() != null) {
                 result.add(artifact);
             }
         }
         return result;
     }
 
-    private void installArtifacts(List<ArtifactInfo> activeArtifacts, Path repoDirectory) {
+    private void installArtifacts(List<ArtifactInfo> artifacts, Path repoDirectory, String version) {
         int exitCode;
-        for (ArtifactInfo artifact : activeArtifacts) {
-            Commandline installCommandLine = ShellHelper.getInstallCommand(settings, artifact, repoDirectory);
-            LOG.info(SHELL_COMMAND_LOG_FORMAT, repoDirectory, installCommandLine);
-            exitCode = runCommand(installCommandLine);
-            Assert.assertEquals(
-                "Installation of " + artifact + " failed with code " + exitCode,
-                0,
-                exitCode);
+        for (ArtifactInfo artifact : artifacts) {
+            try {
+                VersionUpdateHelper.update(artifact, version);
+            } catch (IOException e) {
+                throw new AssertionError("Could not update " + artifact.getPath(), e);
+            }
+            Commandline installCommand = ShellHelper.getInstallCommand(settings, artifact, repoDirectory);
+            LOG.info(SHELL_COMMAND_FORMAT, installCommand.getWorkingDirectory(), installCommand);
+            exitCode = runCommand(installCommand);
+            Assert.assertEquals(String.format(OPERATION_FAILED, "Installation of " + artifact), 0, exitCode);
         }
     }
 
-    private void uninstallArtifacts(List<ArtifactInfo> artifacts, Path repoDirectory) {
-        Commandline purgeCommandLine = ShellHelper.getPurgeCommand(settings, artifacts);
-        LOG.info(SHELL_COMMAND_LOG_FORMAT, repoDirectory, purgeCommandLine);
-        int exitCode = runCommand(purgeCommandLine);
-        Assert.assertEquals(
-            "Purging of the former EAK assembly failed with code " + exitCode,
-            0,
-            exitCode);
+    private void uninstallArtifacts(List<ArtifactInfo> artifacts) {
+        Commandline purgeCommand = ShellHelper.getPurgeCommand(settings, artifacts);
+        LOG.info(SHELL_COMMAND_FORMAT, purgeCommand.getWorkingDirectory(), purgeCommand);
+        int exitCode = runCommand(purgeCommand);
+        Assert.assertEquals(String.format(OPERATION_FAILED,"Purge of the former EAK assembly"),0, exitCode);
     }
 
     private static int runCommand(Commandline command) {
