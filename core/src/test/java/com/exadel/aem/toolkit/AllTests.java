250029dd5a9b3eb803695559585f210655d975a3
Stepan Miakchilo <smiakchilo@exadel.com>
2024-06-01 00:39:35 +0300

    [EAK-527] Moved the AllTests class

--- core/src/test/java/com/exadel/aem/toolkit/AllTests.java
+++ core/src/test/java/com/exadel/aem/toolkit/AllTests.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.exadel.aem.toolkit.core;
+package com.exadel.aem.toolkit;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
