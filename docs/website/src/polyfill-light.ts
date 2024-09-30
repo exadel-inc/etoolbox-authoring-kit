3af17afcbeb0551065014ca73ccbfb2e43274cd1
alesun <alesun@exadel.com>
2024-09-30 16:23:52 +0200

    [EAK-549] Website: return smoothscroll-polyfill

--- docs/website/src/polyfill-light.ts
+++ docs/website/src/polyfill-light.ts
@@ -1,2 +1,5 @@
 import {ResizeObserver} from '@juggle/resize-observer';
 window.ResizeObserver = window.ResizeObserver || ResizeObserver;
+
+import SmoothScroll from 'smoothscroll-polyfill';
+SmoothScroll.polyfill();
