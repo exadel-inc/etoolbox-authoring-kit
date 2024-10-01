af0577f935e19fb1a28481315a602524649ff93c
alesun <alesun@exadel.com>
2024-10-01 20:03:13 +0200

    [EAK-549] Eslint: migrate website config to 9 version

---
+++ docs/website/eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.js'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'warn'
+    }
+  },
+
+  ...require('./eslint.config.ignore'),
+  ...require('@exadel/eslint-config-esl').typescript,
+  ...require('@exadel/eslint-config-esl').recommended
+];
