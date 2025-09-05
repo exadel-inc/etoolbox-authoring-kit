4e7a4dd94b67ff417050de62806799a584e35a9a
Stepan Miakchilo <smiakchilo@exadel.com>
2025-09-05 15:55:28 +0300

    [EAK-605] Add unit tests

---
+++ core/src/test/java/com/exadel/aem/toolkit/core/configurator/services/ConfigChangeListenerTest.java
@@ -0,0 +1,362 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.exadel.aem.toolkit.core.configurator.services;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.sling.api.resource.PersistenceException;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.observation.ResourceChange;
+import org.apache.sling.jcr.resource.internal.JcrResourceChange;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import io.wcm.testing.mock.aem.junit.AemContext;
+import junitx.util.PrivateAccessor;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import com.exadel.aem.toolkit.core.AemContextFactory;
+import com.exadel.aem.toolkit.core.CoreConstants;
+import com.exadel.aem.toolkit.core.configurator.ConfiguratorConstants;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConfigChangeListenerTest {
+
+    private static final String FIELD_CONFIG_ADMIN = "configurationAdmin";
+
+    private static final String NODE_DATA = "/data";
+    private static final String NODE_INITIAL = "/initial/";
+
+    private static final Map<String, Object> CONFIG_ENABLED = Collections.singletonMap("enabled", true);
+
+    private static final String TEST_PID = "com.example.test.Config";
+
+    @Rule
+    public AemContext context = AemContextFactory.newInstance();
+
+    @Test
+    public void shouldProcessEnabledProperty() throws Exception {
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        assertNotNull(PrivateAccessor.getField(configChangeListener, "registration"));
+
+        configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            Collections.singletonMap("enabled", false));
+        assertNull(PrivateAccessor.getField(configChangeListener, "registration"));
+    }
+
+    @Test
+    public void shouldCleanUpConfigurationsOnActivate() throws PersistenceException {
+        String configPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID;
+        String backupPath = ConfiguratorConstants.ROOT_PATH + NODE_INITIAL + TEST_PID;
+
+        context.create().resource(configPath);
+        context.create().resource(backupPath);
+        context.resourceResolver().commit();
+
+        assertNotNull(context.resourceResolver().getResource(configPath));
+        assertNotNull(context.resourceResolver().getResource(backupPath));
+
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put("enabled", true);
+        configProps.put("cleanUp", new String[] {TEST_PID, "com.example.test.AnotherConfig"});
+        context.registerInjectActivateService(new ConfigChangeListener(), configProps);
+
+        assertNull(context.resourceResolver().getResource(ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID));
+        assertNull(context.resourceResolver().getResource(ConfiguratorConstants.ROOT_PATH + NODE_INITIAL + TEST_PID));
+    }
+
+    @Test
+    public void shouldUpdateConfigurationUponActivate() throws IOException {
+        String configPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID;
+        context.create().resource(configPath);
+
+        String dataPath = configPath + NODE_DATA;
+        Map<String, Object> props = new HashMap<>();
+        props.put("test.property", "test.value");
+        props.put("test.number", 42);
+        context.create().resource(dataPath, props);
+        context.resourceResolver().commit();
+
+        context.registerInjectActivateService(new ConfigChangeListener(), CONFIG_ENABLED);
+
+        ConfigurationAdmin configurationAdmin = context.getService(ConfigurationAdmin.class);
+        assertNotNull(configurationAdmin);
+        Configuration configuration = configurationAdmin.getConfiguration(TEST_PID);
+        assertNotNull(configuration);
+        assertEquals("test.value", configuration.getProperties().get("test.property"));
+        assertEquals(42, configuration.getProperties().get("test.number"));
+    }
+
+    @Test
+    public void shouldProcessResourceChangeEvents() throws IOException {
+        String configPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID;
+        context.create().resource(configPath);
+
+        String dataPath = configPath + NODE_DATA;
+        Map<String, Object> props = new HashMap<>();
+        props.put("test.property", "updated.value");
+        context.create().resource(dataPath, props);
+        context.resourceResolver().commit();
+
+        ResourceChange change = new JcrResourceChange(
+            ResourceChange.ChangeType.CHANGED,
+            dataPath,
+            false,
+            null);
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+
+        configChangeListener.onChange(Collections.singletonList(change));
+
+        ConfigurationAdmin configurationAdmin = context.getService(ConfigurationAdmin.class);
+        assertNotNull(configurationAdmin);
+        Configuration configuration = configurationAdmin.getConfiguration(TEST_PID);
+        assertNotNull(configuration);
+        assertEquals("updated.value", configuration.getProperties().get("test.property"));
+    }
+
+    @Test
+    public void shouldHandleResourceRemovalEvents() throws IOException {
+        String dataPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID + NODE_DATA;
+        String backupPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + NODE_INITIAL + TEST_PID;
+        Map<String, Object> backupProps = new HashMap<>();
+        backupProps.put("test.property", "original.value");
+        context.create().resource(backupPath, backupProps);
+        context.resourceResolver().commit();
+
+        ResourceChange change = new JcrResourceChange(
+            ResourceChange.ChangeType.REMOVED,
+            dataPath,
+            false,
+            null);
+
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        configChangeListener.onChange(Collections.singletonList(change));
+
+        ConfigurationAdmin configurationAdmin = context.getService(ConfigurationAdmin.class);
+        assertNotNull(configurationAdmin);
+        Configuration configuration = configurationAdmin.getConfiguration(TEST_PID);
+        assertNotNull(configuration);
+        assertEquals("original.value", configuration.getProperties().get("test.property"));
+    }
+
+    @Test
+    public void shouldIgnoreNonAccountableChanges() throws IOException, NoSuchFieldException {
+        ResourceChange rootChange = new JcrResourceChange(
+            ResourceChange.ChangeType.REMOVED,
+            ConfiguratorConstants.ROOT_PATH,
+            false,
+            null);
+        ResourceChange initialChange = new JcrResourceChange(
+            ResourceChange.ChangeType.CHANGED,
+            ConfiguratorConstants.ROOT_PATH + NODE_INITIAL + TEST_PID,
+            false,
+            null);
+        ResourceChange nonDataChange = new JcrResourceChange(
+            ResourceChange.ChangeType.ADDED,
+            ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID,
+            false,
+            null);
+        List<ResourceChange> changes = Arrays.asList(rootChange, initialChange, nonDataChange);
+
+        ConfigurationAdmin mockConfigurationAdmin = Mockito.mock(ConfigurationAdmin.class);
+
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        PrivateAccessor.setField(configChangeListener, FIELD_CONFIG_ADMIN, mockConfigurationAdmin);
+        configChangeListener.onChange(changes);
+
+        Mockito.verify(mockConfigurationAdmin, Mockito.never()).getConfiguration(Mockito.anyString());
+    }
+
+    @Test
+    public void shouldHandleConfigurationAdminException() throws IOException, NoSuchFieldException {
+        ConfigurationAdmin mockConfigurationAdmin = Mockito.mock(ConfigurationAdmin.class);
+        Mockito.when(mockConfigurationAdmin.getConfiguration(TEST_PID))
+            .thenThrow(new IOException("Test configuration exception"));
+
+        String dataPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID + NODE_DATA;
+        context.create().resource(dataPath);
+        context.resourceResolver().commit();
+
+        ResourceChange change = new JcrResourceChange(
+            ResourceChange.ChangeType.CHANGED,
+            dataPath,
+            false,
+            null);
+
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        PrivateAccessor.setField(configChangeListener, FIELD_CONFIG_ADMIN, mockConfigurationAdmin);
+        configChangeListener.onChange(Collections.singletonList(change));
+
+        Mockito.verify(mockConfigurationAdmin).getConfiguration(TEST_PID);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldUpdateForeignConfigurationBundleLocation() throws Exception {
+        String foreignPid = "foreign.bundle.Config";
+        String foreignBundleLocation = "test";
+        Configuration foreignConfig = Mockito.mock(Configuration.class);
+        ConfigurationAdmin mockConfigurationAdmin = Mockito.mock(ConfigurationAdmin.class);
+        Mockito.when(mockConfigurationAdmin.getConfiguration(foreignPid)).thenReturn(foreignConfig);
+        Mockito.when(foreignConfig.getBundleLocation()).thenReturn(foreignBundleLocation);
+
+        String dataPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + foreignPid + NODE_DATA;
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put("foreign.property", "foreign.value");
+
+        context.create().resource(dataPath, configProps);
+        context.resourceResolver().commit();
+
+        ResourceChange change = new JcrResourceChange(
+            ResourceChange.ChangeType.CHANGED,
+            dataPath,
+            false,
+            null);
+
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        PrivateAccessor.setField(configChangeListener, FIELD_CONFIG_ADMIN, mockConfigurationAdmin);
+        configChangeListener.onChange(Collections.singletonList(change));
+
+        Mockito.verify(foreignConfig).setBundleLocation("?" + foreignBundleLocation);
+        Mockito.verify(foreignConfig).update(Mockito.any(Dictionary.class));
+    }
+
+    @Test
+    public void shouldCreateBackup() throws Exception {
+        Dictionary<String, Object> existingProps = new Hashtable<>();
+        existingProps.put("existing.property", "existing.value");
+        Configuration mockConfig = Mockito.mock(Configuration.class);
+        Mockito.when(mockConfig.getProperties()).thenReturn(existingProps);
+
+        ConfigurationAdmin mockConfigurationAdmin = Mockito.mock(ConfigurationAdmin.class);
+        Mockito.when(mockConfigurationAdmin.getConfiguration(TEST_PID)).thenReturn(mockConfig);
+
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        PrivateAccessor.setField(configChangeListener, FIELD_CONFIG_ADMIN, mockConfigurationAdmin);
+
+        String dataPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID + NODE_DATA;
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put("new.property", "new.value");
+
+        context.create().resource(dataPath, configProps);
+        context.resourceResolver().commit();
+
+        ResourceChange change = new JcrResourceChange(
+            ResourceChange.ChangeType.CHANGED,
+            dataPath,
+            false,
+            null);
+        configChangeListener.onChange(Collections.singletonList(change));
+
+        String backupPath = ConfiguratorConstants.ROOT_PATH + NODE_INITIAL + CoreConstants.SEPARATOR_SLASH + TEST_PID;
+        Resource backupResource = context.resourceResolver().getResource(backupPath);
+        assertNotNull(backupResource);
+        assertEquals("existing.value", backupResource.getValueMap().get("existing.property"));
+    }
+
+    @Test
+    public void shouldNotCreateBackupWhenAlreadyExists() throws Exception {
+        Dictionary<String, Object> existingProps = new Hashtable<>();
+        existingProps.put("existing.property", "existing.value");
+        Configuration mockConfig = Mockito.mock(Configuration.class);
+
+        ConfigurationAdmin mockConfigurationAdmin = Mockito.mock(ConfigurationAdmin.class);
+        Mockito.when(mockConfigurationAdmin.getConfiguration(TEST_PID)).thenReturn(mockConfig);
+
+        String backupPath = ConfiguratorConstants.ROOT_PATH + NODE_INITIAL + TEST_PID;
+        Map<String, Object> backupProperties = new HashMap<>();
+        backupProperties.put("original.property", "original.value");
+        context.create().resource(backupPath, backupProperties);
+        context.resourceResolver().commit();
+
+        Resource backupResource = context.resourceResolver().getResource(backupPath);
+        assertNotNull(backupResource);
+
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        PrivateAccessor.setField(configChangeListener, FIELD_CONFIG_ADMIN, mockConfigurationAdmin);
+
+        String dataPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID + NODE_DATA;
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put("new.property", "new.value");
+        context.create().resource(dataPath, configProps);
+        context.resourceResolver().commit();
+
+        ResourceChange change = new JcrResourceChange(
+            ResourceChange.ChangeType.CHANGED,
+            dataPath,
+            false,
+            null);
+        configChangeListener.onChange(Collections.singletonList(change));
+
+        backupResource = context.resourceResolver().getResource(backupPath);
+        assertNotNull(backupResource);
+        assertEquals("original.value", backupResource.getValueMap().get("original.property"));
+    }
+
+    @Test
+    public void shouldHandlePersistenceExceptionDuringBackupDeletion() throws Exception {
+        Configuration mockConfig = Mockito.mock(Configuration.class);
+        ConfigurationAdmin mockConfigurationAdmin = Mockito.mock(ConfigurationAdmin.class);
+        Mockito.when(mockConfigurationAdmin.getConfiguration(TEST_PID)).thenReturn(mockConfig);
+
+        String backupPath = ConfiguratorConstants.ROOT_PATH + NODE_INITIAL + TEST_PID;
+        context.create().resource(backupPath);
+        context.resourceResolver().commit();
+
+        String dataPath = ConfiguratorConstants.ROOT_PATH + CoreConstants.SEPARATOR_SLASH + TEST_PID + NODE_DATA;
+        ResourceChange change = new JcrResourceChange(
+            ResourceChange.ChangeType.CHANGED,
+            dataPath,
+            false,
+            null);
+
+        ConfigChangeListener configChangeListener = context.registerInjectActivateService(
+            new ConfigChangeListener(),
+            CONFIG_ENABLED);
+        PrivateAccessor.setField(configChangeListener, FIELD_CONFIG_ADMIN, mockConfigurationAdmin);
+        configChangeListener.onChange(Collections.singletonList(change));
+
+        Mockito.verify(mockConfigurationAdmin).getConfiguration(TEST_PID);
+    }
+}
---
+++ core/src/test/java/com/exadel/aem/toolkit/core/configurator/services/MapUtilTest.java
@@ -0,0 +1,181 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.exadel.aem.toolkit.core.configurator.services;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class MapUtilTest {
+
+    @Test
+    public void shouldConvertValueMapToDictionary() {
+        Map<String, Object> sourceMap = new HashMap<>();
+        sourceMap.put("string.property", "test value");
+        sourceMap.put("int.property", 42);
+        sourceMap.put("boolean.property", true);
+        sourceMap.put("double.property", 3.14);
+
+        sourceMap.put("string.array", new String[]{"value1", "value2"});
+        sourceMap.put("int.array", new int[]{1, 2, 3});
+        sourceMap.put("empty.array", new String[0]);
+
+        sourceMap.put("string.list", Arrays.asList("value1", "value2"));
+        sourceMap.put("int.list", Arrays.asList(1, 2, 3));
+        sourceMap.put("mixed.list", Arrays.asList("string", 42, true));
+        sourceMap.put("null.list", Arrays.asList("value", null, "another"));
+        sourceMap.put("empty.list", Collections.emptyList());
+
+        ValueMap valueMap = new ValueMapDecorator(sourceMap);
+
+        Dictionary<String, ?> result = MapUtil.toDictionary(valueMap);
+        assertNotNull(result);
+
+        assertEquals("test value", result.get("string.property"));
+        assertEquals(42, result.get("int.property"));
+        assertEquals(true, result.get("boolean.property"));
+        assertEquals(3.14, result.get("double.property"));
+
+        assertEquals(2, Array.getLength(result.get("string.array")));
+        assertEquals(3, Array.getLength(result.get("int.array")));
+        assertEquals(0, Array.getLength(result.get("empty.array")));
+
+        assertEquals(2, CollectionUtils.size(result.get("string.list")));
+        assertEquals(3, CollectionUtils.size(result.get("int.list")));
+        assertEquals(3, CollectionUtils.size(result.get("mixed.list")));
+        assertNull(result.get("null.list")); // Should be filtered out due to null element
+        assertEquals(0, CollectionUtils.size(result.get("empty.list")));
+    }
+
+    @Test
+    public void shouldExcludeSystemProperties() {
+        Map<String, Object> sourceMap = new HashMap<>();
+        sourceMap.put("jcr:primaryType", "nt:unstructured");
+        sourceMap.put("jcr:created", "2023-01-01");
+        sourceMap.put("sling:resourceType", "test/component");
+        sourceMap.put("normal.property", "test value");
+        ValueMap valueMap = new ValueMapDecorator(sourceMap);
+
+        Dictionary<String, ?> result = MapUtil.toDictionary(valueMap);
+        assertNotNull(result);
+
+        assertEquals("test value", result.get("normal.property"));
+        assertNull(result.get("jcr:primaryType"));
+        assertNull(result.get("jcr:created"));
+        assertNull(result.get("sling:resourceType"));
+    }
+
+    @Test
+    public void shouldExcludeUnsupportedTypesFromDictionary() {
+        Map<String, Object> sourceMap = new HashMap<>();
+        sourceMap.put("string.property", "valid");
+        sourceMap.put("object.property", new Object());
+        sourceMap.put("null.property", null);
+        ValueMap valueMap = new ValueMapDecorator(sourceMap);
+
+        Dictionary<String, ?> result = MapUtil.toDictionary(valueMap);
+        assertNotNull(result);
+
+        assertEquals("valid", result.get("string.property"));
+        assertNull(result.get("object.property"));
+        assertNull(result.get("null.property"));
+    }
+
+    @Test
+    public void shouldConvertDictionaryToValueMap() {
+        Dictionary<String, Object> dictionary = new Hashtable<>();
+        dictionary.put("string.property", "test value");
+        dictionary.put("int.property", 42);
+        dictionary.put("boolean.property", true);
+        dictionary.put("double.property", 3.14);
+
+        dictionary.put("string.array", new String[]{"value1", "value2"});
+        dictionary.put("int.array", new int[]{1, 2, 3});
+
+        dictionary.put("string.list", Arrays.asList("value1", "value2"));
+        dictionary.put("int.list", Arrays.asList(1, 2, 3));
+        dictionary.put("mixed.list", Arrays.asList("string", 42, true));
+        dictionary.put("null.list", Arrays.asList("value", null, "another"));
+
+        Map<String, Object> result = MapUtil.toMap(dictionary);
+        assertNotNull(result);
+
+        assertEquals("test value", result.get("string.property"));
+        assertEquals(42, result.get("int.property"));
+        assertEquals(Boolean.TRUE, result.get("boolean.property"));
+        assertEquals(3.14, result.get("double.property"));
+
+        assertEquals(2, Array.getLength(result.get("string.array")));
+        assertEquals(3, Array.getLength(result.get("int.array")));
+
+        assertEquals(2, CollectionUtils.size(result.get("string.list")));
+        assertEquals(3, CollectionUtils.size(result.get("int.list")));
+        assertEquals(3, CollectionUtils.size(result.get("mixed.list")));
+        assertNull(result.get("null.list")); // Should be filtered out due to null element
+    }
+
+    @Test
+    public void shouldExcludeServiceProperties() {
+        Dictionary<String, Object> dictionary = new Hashtable<>();
+        dictionary.put("service.pid", "com.example.service");
+        dictionary.put("service.factoryPid", "com.example.factory");
+        dictionary.put("normal.property", "test value");
+
+        Map<String, Object> result = MapUtil.toMap(dictionary);
+        assertNotNull(result);
+        assertEquals("test value", result.get("normal.property"));
+        assertNull(result.get("service.pid"));
+        assertNull(result.get("service.factoryPid"));
+    }
+
+    @Test
+    public void shouldExcludeUnsupportedTypesFromMap() {
+        Dictionary<String, Object> dictionary = new Hashtable<>();
+        dictionary.put("string.property", "valid");
+        dictionary.put("object.property", new Object());
+
+        Map<String, Object> result = MapUtil.toMap(dictionary);
+        assertNotNull(result);
+        assertEquals("valid", result.get("string.property"));
+        assertNull(result.get("object.property"));
+    }
+
+    @Test
+    public void shouldHandleEmptyDictionary() {
+        Dictionary<String, Object> dictionary = new Hashtable<>();
+
+        Map<String, Object> result = MapUtil.toMap(dictionary);
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+
+        dictionary = null;
+
+        result = MapUtil.toMap(dictionary);
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+}
