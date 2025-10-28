ff52878a1f114f0bb091cba1f4d8ebbffff15fdb
Stepan Miakchilo <smiakchilo@exadel.com>
2025-10-28 15:02:22 +0300

    [EAK-619] Reuse a constant

--- core/src/test/java/com/exadel/aem/toolkit/core/configurator/servlets/FieldUtilTest.java
+++ core/src/test/java/com/exadel/aem/toolkit/core/configurator/servlets/FieldUtilTest.java
@@ -167,7 +167,7 @@ public class FieldUtilTest {
             .filter(res -> res.getValueMap().get(CoreConstants.PN_NAME, String.class) != null)
             .map(res -> res.getValueMap().get(CoreConstants.PN_NAME, String.class)
                 + "=" + res.getValueMap().get(CoreConstants.PN_VALUE, String.class))
-            .filter(str -> !str.contains("@"))
+            .filter(str -> !str.contains(CoreConstants.SEPARATOR_AT))
             .toArray(String[]::new);
         assertArrayEquals(
             new String[]{
