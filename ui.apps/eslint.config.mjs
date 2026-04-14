7e2260e2d4a78f07ff300d6c94c1950ef537a940
Liubou Masiuk <lmasiuk@exadel.com>
2026-04-14 14:45:35 +0300

    [EAK-649] update eslint and clientlib config to include more recent js versions

--- ui.apps/eslint.config.mjs
+++ ui.apps/eslint.config.mjs
@@ -10,7 +10,7 @@ export default [
             'no-useless-call': 'off'
         },
         languageOptions: {
-            ecmaVersion: 2022,
+            ecmaVersion: 'latest',
             sourceType: 'module',
             parserOptions: {
                 projectService: true
